545c95671a5ff06eace84ea0846023b79d374a7e
Jieyang Chen <cjy7117@gmail.com>
2018-02-15 09:14:54 -0800

    test

--- install_on_travis.sh
+++ install_on_travis.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 # Install necessary libraries
 pip install pyro4 --user
 pip install boto3 --user
@@ -22,10 +23,11 @@ mkdir ~/.bee/tmp
 
 
 # Create SSH Key
-ssh-keygen -t rsa -f ~/.bee/ssh_key/id_rsa -q -P ""
+cp ./travis_ci/id_rsa ~/.bee/ssh_key/id_rsa
 chmod 600 ~/.bee/ssh_key/id_rsa
 
 # Add authorized_keys
+cp ./travis_ci/id_rsa.pub ~/.bee/ssh_key/id_rsa.pub
 cat ~/.bee/ssh_key/id_rsa.pub >> ~/.bee/ssh_key/authorized_keys
 
 # Setup SSH config
@@ -33,3 +35,5 @@ echo "Host *" >> ~/.bee/ssh_key/config
 echo "     Port 22" >> ~/.bee/ssh_key/config
 echo "     StrictHostKeyChecking no" >> ~/.bee/ssh_key/config
 echo "     UserKnownHostsFile=/dev/null" >> ~/.bee/ssh_key/config
+
+export PATH=$(pwd)/bee-launcher:$PATH
