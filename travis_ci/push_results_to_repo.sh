7f076a038aad3c2a98ebefa9d7a497ecf51217a1
Jieyang Chen <cjy7117@gmail.com>
2018-03-02 12:40:34 -0800

    test

---
+++ travis_ci/push_results_to_repo.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout -b ${TRAVIS_BRANCH}
+  touch scaliability_result_${TRAVIS_BUILD_NUMBER}.csv
+  git add . scaliability_result_${TRAVIS_BUILD_NUMBER}.csv
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin ${TRAVIS_BRANCH} 
+}
+
+setup_git
+commit_website_files
+upload_files
