009a21e27e964d1ee2b1a017ec21788c6353feb1
Jieyang Chen <cjy7117@gmail.com>
2018-05-16 21:34:10 -0700

    update examples

--- examples/bee-composer-example/blast/bee-aws/blast-split.sh
+++ examples/bee-composer-example/blast/bee-aws/blast-split.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-#cp /root/makeflow-examples/blast/small.fasta /mnt/blast/
-cd /mnt/blast
-#wget ftp://ftp.ncbi.nlm.nih.gov/blast/db/nt.44.tar.gz
-#mkdir nt
-#tar -C nt -xvzf nt.44.tar.gz
-/root/makeflow-examples/blast/split_fasta 100 small.fasta
+cp /home/beeuser/makeflow-examples/blast/small.fasta /mnt/docker_share/
+/home/beeuser/makeflow-examples/blast/split_fasta 100 /mnt/docker_share/small.fasta
