3c96ab8dcd8bc8d2b108494a2b56e872c1a7d7c9
Paul Bryant <pbryant@lanl.gov>
2018-07-17 14:20:51 -0400

    Changed "delete_after_exec" to false to help with show example has run correctly.

--- examples/bee-composer-example/blast/bee-cc/blast-output-err.sh
+++ examples/bee-composer-example/blast/bee-cc/blast-output-err.sh
@@ -12,5 +12,5 @@ else
     BLAST_OUT=$1
 fi
 
-ch-run -b $BLAST_OUT $BLAST_CH -- sh -c "cat /mnt/0/input.fasta.0.err \
+ch-run --no-home -b $BLAST_OUT $BLAST_CH -- sh -c "cat /mnt/0/input.fasta.0.err \
     /mnt/0/input.fasta.1.err > /mnt/0/output.fasta.err"
--- examples/bee-composer-example/blast/bee-cc/blast-worker002.sh
+++ examples/bee-composer-example/blast/bee-cc/blast-worker002.sh
@@ -13,6 +13,6 @@ else
     BLAST_OUT=$1
 fi
 
-ch-run -b $BLAST_OUT $BLAST_CH -- $BLAST_LOC/blastall -p blastn \
+ch-run --no-home -b $BLAST_OUT $BLAST_CH -- $BLAST_LOC/blastall -p blastn \
     -d nt/nt -i /mnt/0/small.fasta.1 -o /mnt/0/input.fasta.1.out \
     -l /mnt/0/input.fasta.1.err
