884d7ace2a0b98ec7bff7d3f038c64ffc3c600b1
Paul Bryant <pbryant@lanl.gov>
2018-07-17 15:17:56 -0400

    Updated lammps_cc_gen_run.sh to reflect changes in `general_run` support

--- examples/bee-charliecloud-examples/lammps/lammps_cc_gen_run.sh
+++ examples/bee-charliecloud-examples/lammps/lammps_cc_gen_run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# BEE Charliecloud example utilizing LAMMPS via single node
+
 # mpirun -np 32 ch-run \ #if you want to specify using all cpus (default)
 # or any flag to specify hosts, mapping by node etc.
 
@@ -7,10 +9,15 @@
 # the modules need to be loaded before the allocation in the same window as
 # bee_orc_ctl.py is run and the allocation needs to be done in that window
 # or a path set to openmpi and charliecloud
-mkdir -p out
-mpirun ch-run \
+
+LAMMPS_OUT=out/ # output directory for LAMMPS data
+                # removed prior to start of example!
+
+rm -rf $LAMMPS_OUT
+mkdir -p $LAMMPS_OUT
+
+ch-run \
  -b out \
  /var/tmp/lammps_example \
  -- /lammps/src/lmp_mpi \
  -in /lammps/examples/melt/in.melt -log /mnt/0/lammps_gen.log # has larger region 50 steps
-
