58a340c73f663fae57bde4ba86ef723c33f69575
Paul Bryant <pbryant@lanl.gov>
2018-08-01 09:10:54 -0600

    Updates to vpic-paraview for Charliecloud, associated with Jeiyang's paper

--- examples/bee-launcher-example/vpic-paraview/bee-cc/seq-tom-vpic.sh
+++ examples/bee-launcher-example/vpic-paraview/bee-cc/seq-tom-vpic.sh
@@ -4,4 +4,5 @@ VPIC_CH=/var/tmp/vpic
 VPIC_OUT=~/vpic_share
 LAUNCH_LOC=/home/beeuser/launch.sh
 
-ch-run --no-home -b $VPIC_OUT:/mnt/docker_share $VPIC_CH -- sh $LAUNCH_LOC
+ch-run --no-home -b $VPIC_OUT:/mnt/docker_share $VPIC_CH -- \
+	/home/beeuser/./launch.sh
