2cbb3f33768c7275ec8da106ac9995f9ed585448
Patricia Grubel <pagrubel@lanl.gov>
2018-11-06 17:24:20 -0700

    add use_existing_image feature to beefile for Charliecloud

---
+++ examples/bee-composer-example/blast-repeat/bee-cc/blast-output-err.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# BEE Charliecloud example utilizing blast
+# Argument #1   Blast output/share directory
+#               Must remain constant across flow
+
+BLAST_CH=/var/tmp/blast
+
+if [ -z "$1" ]; then
+    BLAST_OUT=~/blast_output
+else
+    BLAST_OUT=$1
+fi
+
+ch-run --no-home -b $BLAST_OUT $BLAST_CH -- sh -c "cat /mnt/0/input.fasta.0.err \
+    /mnt/0/input.fasta.1.err > /mnt/0/output.fasta.err"
---
+++ examples/bee-composer-example/blast-repeat/bee-cc/blast-output2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# BEE Charliecloud example utilizing blast
+# Argument #1   Blast output/share directory
+#               Must remain constant across flow
+
+BLAST_CH=/var/tmp/blast2
+BLAST_LOC=/home/beeuser/makeflow-examples/blast
+
+if [ -z "$1" ]; then
+    BLAST_OUT=~/blast_output2
+else
+    BLAST_OUT=$1
+fi
+
+ch-run --no-home -b $BLAST_OUT $BLAST_CH -- $BLAST_LOC/cat_blast \
+    /mnt/0/output.fasta /mnt/0/input.fasta.0.out /mnt/0/input.fasta.1.out
