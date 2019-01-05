d04c2d085b5ae9de5ab1541f2385fca6bd72c0c2
Patricia Grubel <pagrubel@lanl.gov>
2019-01-05 13:38:09 -0700

    blast example on one node for tutorial (uses existing cc image for dependent tasks)

---
+++ examples/bee-charliecloud-examples/tutorial/blast/blast-worker001.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# BEE Charliecloud example utilizing blast
+# Argument #1   Blast output/share directory
+#               Must remain constant across flow
+
+BLAST_CH=/var/tmp/blast
+BLAST_LOC=/home/beeuser/makeflow-examples/blast
+
+if [ -z "$1" ]; then
+    BLAST_OUT=~/blast_output
+else
+    BLAST_OUT=$1
+fi
+
+ch-run --no-home -b $BLAST_OUT $BLAST_CH -- sh -c "$BLAST_LOC/blastall -p blastn \
+	-d $BLAST_LOC/nt/nt -i /mnt/0/small.fasta.0 -o /mnt/0/input.fasta.0.out \
+	2> /mnt/0/input.fasta.0.err"
