f54cf9c2daa6678616de8d361337eafad24806a9
Steven Anaya <sanaya@lanl.gov>
2019-07-05 14:52:54 -0600

    Implement Neo4j driver methods

--- beeflow/common/gdb/neo4j_driver.py
+++ beeflow/common/gdb/neo4j_driver.py
@@ -28,6 +28,7 @@ class Neo4jDriver(GraphDatabaseDriver):
         """
         try:
             self._driver = Neo4jDatabase.driver(uri, auth=(user, password))
+            self._workflows = []
         except ServiceUnavailable:
             print("Neo4j database unavailable. Is it running?")
 
@@ -44,13 +45,24 @@ class Neo4jDriver(GraphDatabaseDriver):
         # Commit the query transaction in a Neo4j session
         with self._driver.session() as session:
             session.run(cypher_query)
-        self.close()
+
+        self._workflows.append(workflow)
 
     def initialize_workflow_dags(self):
         """Initialize the workflow loaded into the Neo4j database."""
+        READY_QUERY = ('MATCH (t) WHERE NOT (t:Task)-[:DEPENDS]->() '
+                       'SET t.state = "READY"')
+
+        with self._driver.session() as session:
+            session.run(READY_QUERY)
 
     def start_ready_tasks(self):
         """Start tasks that have no unsatisfied dependencies."""
+        START_QUERY = ('MATCH (t:Task {state: "READY"}) '
+                       'SET t.state = "RUNNING"')
+
+        with self._driver.session() as session:
+            session.run(START_QUERY)
 
     def watch_tasks(self):
         """Watch tasks for completion/failure and start new ready tasks."""
@@ -62,20 +74,37 @@ class Neo4jDriver(GraphDatabaseDriver):
         :type task: Task object
         :rtype: set of Task objects
         """
+        DEP_QUERY = ('MATCH (:Task {name: "$name"})<-[:DEPENDS]-(t:Task) '
+                     'RETURN t')
 
-    def get_task_status(self, task):
+        with self._driver.session() as session:
+            deps = session.run(DEP_QUERY, name=task.name).single().value()
+
+        return deps
+
+    def get_task_state(self, task):
         """Get the status of a task in the Neo4j workflow DAG.
 
         :param task: the task whose status to retrieve
         :type task: instance of Task
         :rtype: a string
         """
+        STATUS_QUERY = 'MATCH (t:Task {name: "$name"}) RETURN t.state'
+
+        with self._driver.session() as session:
+            state = session.run(STATUS_QUERY, name=task.name).single().value()
+
+        return state
 
     def finalize_workflow_dags(self):
         """Finalize the workflow DAG loaded into the Neo4j database."""
 
     def cleanup(self):
         """Clean up all data in the Neo4j database."""
+        CLEANUP_QUERY = 'MATCH (n) DETACH DELETE n'
+
+        with self._driver.session() as session:
+            session.run(CLEANUP_QUERY)
 
     def close(self):
         """Close the connection to the Neo4j database."""
@@ -83,7 +112,11 @@ class Neo4jDriver(GraphDatabaseDriver):
 
     # Cypher statement construction helpers
     def _construct_create_statements(self, tasks):
-        """Construct a series of CREATE statements for the tasks."""
+        """Construct a series of CREATE statements for the tasks.
+
+        :param tasks: the new tasks to add
+        :type tasks: list of Task instances
+        """
         CREATE_TEMP = Template(
                 'CREATE ($task_id:Task {name:"$name", state:"WAITING"})')
         create_stmts = [CREATE_TEMP.substitute(task_id=task.id, name=task.name)
@@ -91,7 +124,11 @@ class Neo4jDriver(GraphDatabaseDriver):
         return "\n".join(create_stmts) + "\n"
 
     def _construct_merge_statements(self, tasks):
-        """Construct a series of MERGE statements for task dependencies."""
+        """Construct a series of MERGE statements for task dependencies.
+
+        :param tasks: the tasks whose dependencies to add as relationships
+        :type tasks: list of Task instances
+        """
         DEP_TEMP = Template("MERGE ($dependent)-[:DEPENDS]->($dependency)")
         merge_stmts = [DEP_TEMP.substitute(dependent=task.id,
                                            dependency=dependency)
