cfadab4599ee870967f01fbdd169ade7e4a8e47c
Steven Anaya <sanaya@lanl.gov>
2019-07-09 15:26:13 -0600

    Remove/revert single quotes

--- beeflow/common/gdb/neo4j_cypher.py
+++ beeflow/common/gdb/neo4j_cypher.py
@@ -3,8 +3,8 @@
 
 def constrain_tasks_unique(tx):
     """Constrain tasks to have unique names."""
-    unique_query = ('CREATE CONSTRAINT ON (t:Task) '
-                    'ASSERT t.name IS UNIQUE')
+    unique_query = ("CREATE CONSTRAINT ON (t:Task) "
+                    "ASSERT t.name IS UNIQUE")
 
     tx.run(unique_query)
 
@@ -15,12 +15,12 @@ def create_task(tx, task):
     :param task: the new task to create
     :type task: instance of Task
     """
-    create_query = ('CREATE (t:Task) '
-                    'SET t.name = "$name" '
-                    'SET t.base_command = "$base_command" '
-                    'SET t.arguments = $arguments '
-                    'SET t.dependencies = $dependencies '
-                    'SET t.requirements = $requirements')
+    create_query = ("CREATE (t:Task) "
+                    "SET t.name = $name "
+                    "SET t.base_command = $base_command "
+                    "SET t.arguments = $arguments "
+                    "SET t.dependencies = $dependencies "
+                    "SET t.requirements = $requirements")
 
     tx.run(create_query, name=task.name, base_command=task.base_command, arguments=task.arguments,
            dependencies=list(task.dependencies), requirements=task.requirements)
@@ -32,25 +32,25 @@ def add_dependencies(tx, task):
     :param task: the task whose dependencies to add
     :type task: instance of Task
     """
-    dependency_query = ('MERGE (s:Task)-[:DEPENDS]->(t:Task) '
-                        'WHERE s.name = "$dependent_name" AND t.name = "$dependency_name"')
+    dependency_query = ("MERGE (s:Task)-[:DEPENDS]->(t:Task) "
+                        "WHERE s.name = $dependent_name AND t.name = $dependency_name")
 
     for dependency in task.dependencies:
         tx.run(dependency_query, dependent_name=task.name, dependency_name=dependency.name)
 
 
 def set_head_tasks_to_ready(tx):
-    """Initialize the workflow by setting the head tasks' state to READY."""
-    ready_query = ('MATCH (t:Task) WHERE NOT (t)-[:DEPENDS]->() '
-                   'SET t.state = "READY"')
+    """Initialize the workflow by setting the head tasks" state to READY."""
+    ready_query = ("MATCH (t:Task) WHERE NOT (t)-[:DEPENDS]->() "
+                   "SET t.state = READY")
 
     tx.run(ready_query)
 
 
 def set_ready_tasks_to_running(tx):
     """Set tasks with state READY to RUNNING."""
-    running_query = ('MATCH (t:Task {state: "READY"}) '
-                     'SET t.state = "RUNNING"')
+    running_query = ("MATCH (t:Task {state: READY}) "
+                     "SET t.state = RUNNING")
 
     tx.run(running_query)
 
@@ -61,8 +61,8 @@ def set_task_to_complete(tx, task):
     :param task: the task whose state to change to COMPLETE
     :type task: instance of Task
     """
-    complete_query = ('MATCH (t:Task {name: "$name"}) '
-                      'SET t.state = "COMPLETE"')
+    complete_query = ("MATCH (t:Task {name: $name}) "
+                      "SET t.state = COMPLETE")
 
     tx.run(complete_query, name=task.name)
 
@@ -73,8 +73,8 @@ def set_task_to_failed(tx, task):
     :param task: the task whose state to change to FAILED
     :type task: instance of Task
     """
-    failed_query = ('MATCH (t:Task {name: "$name"}) '
-                    'SET t.state = "FAILED"')
+    failed_query = ("MATCH (t:Task {name: $name}) "
+                    "SET t.state = FAILED")
 
     tx.run(failed_query, name=task.name)
 
@@ -85,9 +85,9 @@ def get_dependent_tasks(tx, task):
     :param task: the task whose dependencies to obtain
     :type task: instance of Task
     """
-    dependents_query = ('MATCH (t:Task)-[:DEPENDS]->(s:Task) '
-                        'WHERE s.name = "$name" '
-                        'RETURN collect(t.name)')
+    dependents_query = ("MATCH (t:Task)-[:DEPENDS]->(s:Task) "
+                        "WHERE s.name = $name "
+                        "RETURN collect(t.name)")
 
     return tx.run(dependents_query, name=task.name).values()
 
@@ -98,15 +98,15 @@ def get_task_state(tx, task):
     :param task: the task whose state to get
     :type task: instance of Task
     """
-    state_query = ('MATCH (t:Task {name: "$name"}) '
-                   'RETURN t.state')
+    state_query = ("MATCH (t:Task {name: $name}) "
+                   "RETURN t.state")
 
     return tx.run(state_query, name=task.name).single().value()
 
 
 def cleanup(tx):
     """Clean up all workflow data in the database."""
-    cleanup_query = ('MATCH (n) WITH n LIMIT 10000 '
-                     'DETACH DELETE n')
+    cleanup_query = ("MATCH (n) WITH n LIMIT 10000 "
+                     "DETACH DELETE n")
 
     tx.run(cleanup_query)
