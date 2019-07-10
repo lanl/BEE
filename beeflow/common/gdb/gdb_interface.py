7e21e34894971059b73bab31c4603911420e7055
Steven Anaya <sanaya@lanl.gov>
2019-07-10 11:15:57 -0600

    Refactor using class methods

--- beeflow/common/gdb/gdb_interface.py
+++ beeflow/common/gdb/gdb_interface.py
@@ -11,37 +11,42 @@ class GraphDatabaseInterface:
     this is the `Neo4jDriver` class.
     """
 
-    def __init__(self, gdb_driver=Neo4jDriver, **kwargs):
+    @classmethod
+    def connect(cls, gdb_driver=Neo4jDriver, **kwargs):
         """Initialize a graph database interface with a driver.
 
         :param gdb_driver: the graph database driver (Neo4jDriver by default)
         :type gdb_driver: subclass of GraphDatabaseDriver
         :param kwargs: optional arguments for the graph database driver
         """
-        self._gdb_driver = gdb_driver(**kwargs)
+        cls._gdb_driver = gdb_driver(**kwargs)
 
-    def load_workflow(self, workflow):
+    @classmethod
+    def load_workflow(cls, workflow):
         """Load a BEE workflow into the graph database.
 
         :param workflow: the new workflow to load
         :type workflow: instance of Workflow
         """
-        self._gdb_driver.load_workflow(workflow)
+        cls._gdb_driver.load_workflow(workflow)
 
-    def initialize_workflows(self):
+    @classmethod
+    def initialize_workflows(cls):
         """Start the workflow loaded into the graph database."""
-        self._gdb_driver.initialize_workflows()
+        cls._gdb_driver.initialize_workflows()
 
-    def get_dependent_tasks(self, task):
+    @classmethod
+    def get_dependent_tasks(cls, task):
         """Get the dependents of a task in a graph database workflow.
 
         :param task: the task whose dependents to obtain
         :type task: instance of Task
         :rtype: set of Task instances
         """
-        return self._gdb_driver.get_dependent_tasks(task)
+        return cls._gdb_driver.get_dependent_tasks(task)
 
-    def get_subworkflow(self, head_tasks):
+    @classmethod
+    def get_subworkflow(cls, head_tasks):
         """Get sub-workflows from the graph database with the specified head tasks.
 
         :param head_tasks: the head tasks of the sub-workflows
@@ -49,6 +54,7 @@ class GraphDatabaseInterface:
         :rtype: instance of Workflow
         """
 
-    def finalize_workflows(self):
+    @classmethod
+    def finalize_workflows(cls):
         """Finalize the BEE workflow loaded into the graph database."""
-        self._gdb_driver.finalize_workflows()
+        cls._gdb_driver.finalize_workflows()
