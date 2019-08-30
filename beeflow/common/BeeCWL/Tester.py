549d945fb551065b8d56b3afcb14b7a740d5f8f4
Betis Baheri <37885817+betisb@users.noreply.github.com>
2019-08-30 01:00:02 -0400

    BeeCWL v1.3 PEP8 Standard, Update On Tester

--- beeflow/common/BeeCWL/Tester.py
+++ beeflow/common/BeeCWL/Tester.py
@@ -7,3 +7,13 @@ print(result)
 p2 = BeeCWL("./blast-cc/blast-cc-flow.cwl")
 result2 = p2.parser
 print(result2)
+
+
+from beeflow.common.wf_interface import WorkflowInterface
+
+name = result._get_value(result.loc[result['__key__']=='class'].index[0],'__value__')
+inputs = result._get_value(result.loc[result['__key__']=='inputs'].index[0],'__value__')
+outputs = result._get_value(result.loc[result['__key__']=='outputs'].index[0],'__value__')
+hints = result._get_value(result.loc[result['__key__']=='requirements'].index[0],'__value__')
+
+Task = WorkflowInterface.create_task(name=name,hints=hints,inputs=inputs,outputs=outputs)
