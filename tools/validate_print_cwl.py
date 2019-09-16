cfcb6f6d44b37189eb82794271324e167f587b81
Patricia Grubel <pagrubel@lanl.gov>
2019-09-16 07:53:22 -0600

    fixed PEP8

--- tools/validate_print_cwl.py
+++ tools/validate_print_cwl.py
@@ -1,47 +1,51 @@
-""" Parses, Validates and Prints a cwl file  """
+""" Parses, Validates and Prints a cwl workflow and sub cwl files. """
 
-import argparse 
-import pprint
-from beeflow.common.wf_interface import WorkflowInterface
-from beeflow.common.parser.cwl_parser import BeeCWL
+def main():
+    '''Entry point if called as an executable'''
 
-cl_parser= argparse.ArgumentParser()
-cl_parser.add_argument('-c','--cwl_file', help='CWL file to parse.', required=True)
-cl_parser.add_argument('-r','--recursive', help='Parse CWL steps files.', action='store_true')
-cl_parser.add_argument('-l','--list', help='Full list of cwl files.', action='store_true')
-args = cl_parser.parse_args()
+    import argparse
+    import pprint
+    from beeflow.common.parser.cwl_parser import BeeCWL
 
-error_files=[]
-cwl_dict=[]
-cwl_file = args.cwl_file
+    cwl_parser = argparse.ArgumentParser()
+    cwl_parser.add_argument('-c', '--cwl_file', help='CWL file to parse.', required=True)
+    cwl_parser.add_argument('-r', '--recursive', help='Parse CWL steps files.', action='store_true')
+    cwl_parser.add_argument('-l', '--list', help='Full list of cwl files.', action='store_true')
+    args = cwl_parser.parse_args()
 
-cwl_bee = BeeCWL(cwl_file)
-cwl_dict = cwl_bee.parser
-if cwl_dict:
-    if args.list: 
-        pp = pprint.PrettyPrinter(indent=4)
-        print('\n Parsing ', cwl_file, '\n')
-        pp.pprint(cwl_dict)
-    
-    if args.recursive :
-         
-        for (step, cwl_file) in (zip(
-                list(cwl_dict.get('steps').keys()),
+    error_files = []
+    cwl_dict = []
+    cwl_file = args.cwl_file
+    pretty_print = pprint.PrettyPrinter(indent=4)
+
+    cwl_bee = BeeCWL(cwl_file)
+    cwl_dict = cwl_bee.parser
+    if cwl_dict:
+        if args.list:
+            print('\n Parsing ', cwl_file, '\n')
+            pretty_print.pprint(cwl_dict)
+
+        if args.recursive:
+
+            for (step, cwl_file) in (zip(
+                    list(cwl_dict.get('steps').keys()),
                     [cwl_dict['steps'][n]['run'] for n in cwl_dict['steps']])):
-            print('\n Step ', step,': Parsing ', cwl_file)
-            cwl_bee = BeeCWL(cwl_file)
-            cwl_dict = cwl_bee.parser
-            if not cwl_dict:
-                error_files.append(cwl_file)   
-            elif args.list: 
-                pp = pprint.PrettyPrinter(indent=4)
-                pp.pprint(cwl_dict)
-elif args.recursive:
-    print('The cwl file has errors, step files cannot be parsed!')
-    error_files.append(cwl_file)
+                print('\n Step ', step, ': Parsing ', cwl_file)
+                cwl_bee = BeeCWL(cwl_file)
+                cwl_dict = cwl_bee.parser
+                if not cwl_dict:
+                    error_files.append(cwl_file)
+                elif args.list:
+                    pretty_print.pprint(cwl_dict)
+    elif args.recursive:
+        print('The cwl file has errors, step files cannot be parsed!')
+        error_files.append(cwl_file)
 
-if error_files:
-    print('\n The following cwl files need to be fixed:')
-    print('\t', *error_files, sep='\n\t')
-else:
-    print('All files are valid!')        
+    if error_files:
+        print('\n The following cwl files need to be fixed:')
+        print('\t', *error_files, sep='\n\t')
+    else:
+        print('All files are valid!')
+
+if __name__ == '__main__':
+    main()
