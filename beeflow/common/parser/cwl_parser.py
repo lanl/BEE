bbf82faf2326de5fde074e5457bd00734c1d9546
Patricia Grubel <pagrubel@lanl.gov>
2019-09-18 15:40:08 -0600

    parse command line args in method

--- beeflow/common/parser/cwl_parser.py
+++ beeflow/common/parser/cwl_parser.py
@@ -7,33 +7,33 @@ from schema_salad import main as schema_salad
 class BeeCWL:
     """Retrieving and validating CWL workflow"""
 
-    def __init__(__cwl__, path):
+    def __init__(self, path):
         """Initialize the parser interface with a CWL workflow location.
 
-    :param __cwl__: the workflow location
-    :type __cwl__: System path (ex. home/user./example/workflow/first_workflow.cwl)
+    :param path: the workflow location
+    :type path: System path (ex. home/user./example/workflow/first_workflow.cwl)
     """
-        __cwl__.path = path
+        self.path = path
 
     # noinspection PyBroadException
     @property
-    def parser(__cwl__):
+    def parser(self):
         """Validates cwl file then returns a dictionary of components in a CWL workflow.
 
            $CWL_VERSIONS required in environment for path to CWL versions for schema.
 
-          :param __cwl__: the orginal CWL location to parse
-          :type __cwl__: System Path
+          :param path: the orginal CWL location to parse
+          :type path: System Path
           """
         try:
-            with open(__cwl__.path, 'r') as cwl_file:
+            with open(self.path, 'r') as cwl_file:
                 cwl_dict = yaml.safe_load(cwl_file)
 
             cwl_version = cwl_dict.get('cwlVersion')
             versions_path = (os.environ['CWL_VERSIONS']
                              + cwl_version
                              + '/CommonWorkflowLanguage.yml')
-            compare = schema_salad.main(argsl=[versions_path, __cwl__.path])
+            compare = schema_salad.main(argsl=[versions_path, self.path])
             if not compare.bit_length() == 0:
                 cwl_dict = None
             return cwl_dict
