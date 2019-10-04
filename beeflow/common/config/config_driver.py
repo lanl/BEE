afc532f35bcc51ae6d23121438a10edc767f7619
Tim Randles <trandles@lanl.gov>
2019-10-04 15:52:49 -0600

    Initial commit of basic config file handler

--- beeflow/common/config/config_driver.py
+++ beeflow/common/config/config_driver.py
@@ -7,15 +7,46 @@ import platform
 
 class BeeConfig:
     """Class to manage and store all BEE configuration.
+
+    BeeConfig.sysconfig is a ConfigParser object of system configurations.
+    BeeConfig.userconfig is a ConfigParser object of user configurations.
+
+    Configuration file locations by supported platform:
+    Linux:
+      sysconfig_file = '/etc/beeflow/bee.conf'
+      userconfig_file = '~/.config/beeflow/bee.conf'
+    MacOS:
+      sysconfig_file = '/Library/Application Support/beeflow/bee.conf'
+      userconfig_file = '~/Library/Application Support/beeflow/bee.conf'
+    Windows:
+      sysconfig_file = NOT SUPPORTED. Should be windows registry.
+      userconfig_file = '%APPDATA%\beeflow\bee.conf'
     """
 
     def __init__(self):
-        """Initialize BeeConfig class.
+        """Initialize BeeConfig class. We check the platform and read in
+        system and user configuration files. 
         """
+
+        self.sysconfig = ConfigParser()
+        self.userconfig = ConfigParser()
         system=platform.system()
         if system == "Linux":
-            config_files=['bee.conf',os.path.expanduser('~/.config/beeflow/bee.conf')]
+            sysconfig_file = '/etc/beeflow/bee.conf'
+            userconfig_file = os.path.expanduser('~/.config/beeflow/bee.conf')
         elif system == "Darwin":
-            config_files=['bee.conf',os.path.expanduser('~/Library/Application Support/beeflow/bee.conf')]
+            sysconfig_file = '/Library/Application Support/beeflow/bee.conf'
+            userconfig_file = os.path.expanduser('~/Library/Application Support/beeflow/bee.conf')
         elif system == "Windows":
-            config_files=['bee.conf',os.path.expandvars(r'%APPDATA%\beeflow\bee.conf'),os.path.expandvars(r'%LOCALAPPDATA%\beeflow\bee.conf')]
+            sysconfig_file = ''
+            userconfig_file = os.path.expandvars(r'%APPDATA%\beeflow\bee.conf')
+
+        try:
+            self.sysconfig.read_file(open(sysconfig_file))
+        except FileNotFoundError:
+            print("System configuration " + sysconfig_file + " not found")
+
+        try:
+            self.userconfig.read_file(open(userconfig_file))
+        except FileNotFoundError:
+            print("User configuration " + userconfig_file + " not found")
