e9cbe5e84ca70dcfade5e653204bff2b657b9c7b
Steven Anaya <sanaya@lanl.gov>
2019-12-17 14:11:57 -0700

    Separate test_numbers into two test cases

--- examples/unittest_tutorial/test_numbers.py
+++ examples/unittest_tutorial/test_numbers.py
@@ -5,10 +5,10 @@ import unittest
 
 
 class TestNumbers(unittest.TestCase):
-    """Example of unit testing with numbers."""
+    """Example of unit testing with integers."""
 
-    def test_relations(self):
-        """Testing relations between numbers."""
+    def test_integers(self):
+        """Testing relations between integers (also works between floats, of course)."""
         # Test if 1 == 1
         self.assertEqual(1, 1)
 
@@ -27,8 +27,18 @@ class TestNumbers(unittest.TestCase):
         # Test if 1 <= 1
         self.assertLessEqual(1, 1)
 
+
+class TestFloats(unittest.TestCase):
+    """Example of unit testing with integers."""
+
+    def test_floats(self):
+        """Testing relations between integers (also works between floats, of course)."""
         # Test if 1 ~= 1.0001 using rounding to decimal places
         self.assertAlmostEqual(1, 1.0001, places=3)
 
         # Test if 1 ~= 1.0001 using delta threshold
         self.assertAlmostEqual(1, 1.0001, delta=0.0001)
+
+
+if __name__ == "__main__":
+    unittest.main()
