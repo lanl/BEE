98d648e4c3fd1786b354585f49596c209e6f6c7c
Timothy C. Randles <trandles@fg-fey1.lanl.gov>
2020-03-26 16:55:53 -0600

    Read in the config section for [graphdb] or create if missing

--- beeflow/server/startup.py
+++ beeflow/server/startup.py
@@ -2,6 +2,26 @@ from beeflow.common.config.config_driver import BeeConfig
 
 bc = BeeConfig()
 
-bolt_port = bc.userconfig['graphdb'].get('bolt_port','7687')
-http_port = bc.userconfig['graphdb'].get('http_port','7474')
-https_port = bc.userconfig['graphdb'].get('https_port','7473')
+if bc.userconfig.has_section('graphdb'):
+    graphsec = bc.userconfig['graphdb']
+    bolt_port = graphsec.get('bolt_port','7687')
+    http_port = graphsec.get('http_port','7474')
+    https_port = graphsec.get('https_port','7473')
+    gdb_img = graphsec.get('gdb_image','')
+    gdb_img_mntdir = graphsec.get('gdb_img_mntdir','/tmp')
+else:
+    print("[graphdb] section not found in user bee.conf.")
+    print("Default values will be added to user bee.conf.")
+    print("Please edit your bee.conf and rerun startup.")
+
+    graphdb_dict = {
+        'name': 'graphdb',
+        'bolt_port': 7687,
+        'http_port': 7474,
+        'https_port': 7473,
+        'gdb_img':
+        'neo4j-ch.tar',
+        'gdb_img_mntdir': '/tmp',
+        }
+
+    bc.add_section('user', graphdb_dict)
