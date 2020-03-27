09bedf1d3595c4ae9fdf830121c833a4e8652949
Tim Randles <trandles@lanl.gov>
2020-03-27 13:08:03 -0600

    Added database hostname to config. Better user messages.

--- beeflow/server/startup.py
+++ beeflow/server/startup.py
@@ -1,21 +1,23 @@
+import sys
+import tempfile
 from beeflow.common.config.config_driver import BeeConfig
 
 bc = BeeConfig()
 
 if bc.userconfig.has_section('graphdb'):
     graphsec = bc.userconfig['graphdb']
+    db_hostname = graphsec.get('hostname','localhost')
     bolt_port = graphsec.get('bolt_port','7687')
     http_port = graphsec.get('http_port','7474')
     https_port = graphsec.get('https_port','7473')
     gdb_img = graphsec.get('gdb_image','')
     gdb_img_mntdir = graphsec.get('gdb_img_mntdir','/tmp')
 else:
-    print("[graphdb] section not found in user bee.conf.")
-    print("Default values will be added to user bee.conf.")
-    print("Please edit your bee.conf and rerun startup.")
+    print("[graphdb] section not found in configuration file, default values will be added.")
 
     graphdb_dict = {
         'name': 'graphdb',
+        'hostname': 'localhost',
         'bolt_port': 7687,
         'http_port': 7474,
         'https_port': 7473,
@@ -25,3 +27,6 @@ else:
         }
 
     bc.add_section('user', graphdb_dict)
+
+    sys.exit("Please check " + str(bc.userconfig_file) + " and rerun startup.")
+
