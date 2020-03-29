3506c7117eefdf4c508ac9f71966614259e5f8a2
Rusty Davis <rustyd@lanl.gov>
2020-03-29 14:57:58 -0600

    Before merging in Al's work

--- beeflow/server/server.py
+++ beeflow/server/server.py
@@ -1,12 +1,14 @@
-#!flask/bin/python
+#!flask/bin/python3
 import os
+import jsonpickle
+import requests
 
 # Server and REST handling
 from flask import Flask, jsonify, make_response
 from flask_restful import Resource, Api, reqparse
 
 # Asynchronous workers
-from celery_setup import make_celery
+#from celery_setup import make_celery
 
 # Interacting with the rm, tm, and scheduler
 from werkzeug.datastructures import FileStorage
@@ -15,23 +17,20 @@ from beeflow.common.wf_interface import WorkflowInterface
 
 flask_app = Flask(__name__)
 # Setup celery 
-flask_app.config.update(
-    CELERY_BROKER_URL='redis://localhost:6379',
-    CELERY_RESULT_BACKEND='redis://localhost:6379'
-)
+#flask_app.config.update(
+#    CELERY_BROKER_URL='redis://localhost:6379',
+#    CELERY_RESULT_BACKEND='redis://localhost:6379'
+#)
 
-celery = make_celery(flask_app)
+#celery = make_celery(flask_app)
 api = Api(flask_app)
 
 UPLOAD_FOLDER = 'workflows'
 flask_app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-wf = WorkflowInterface()
 
-no_file_resp = {
-    'msg':'No file found',
-    'status':'error'
-}
+
+wfi = WorkflowInterface()
 
 # Where we submit jobs
 class JobsList(Resource):
@@ -53,7 +52,8 @@ class JobsList(Resource):
         return resp
 
 # Add workflow to the database
-def add_workflow():
+def add_workflow(filename):
+    tasks = []
     task_input = 'grep.in'
     task_output = 'grep.out'
     grep_string = 'database'
@@ -66,6 +66,8 @@ def add_workflow():
     tasks.append(wfi.add_task(
             'WC', command=['wc', '-l', task_input, '>', task_output],
                 inputs={task_input}, outputs={task_output}))
+    wfi.load_workflow(wf)
+    wfi.initialize_workflow()
 
 class JobSubmit(Resource):
     def __init__(self):
@@ -77,7 +79,7 @@ class JobSubmit(Resource):
         data = self.reqparse.parse_args()
         print("Getting workflow")
         if data['workflow'] == "":
-            return no_file_resp, 201
+            return {'msg':'No file found','status':'error'}, 201
         # Workflow file
         workflow = data['workflow']
 
@@ -105,15 +107,22 @@ class JobActions(Resource):
 
     # Start Job
     def put(self, wf_id):
-        # Send workflow to the scheduler and start running tasks
-        start_wf(wf_id)
-        return "Job is starting"
+        # Send tasks to the task manager
+        # Get tasks from the neo4j database
+        task = get_dependent_task()
+        # Serialize task with json
+        task_msg = jsonpickle.encode(task)
+        # Send task_msg to task manager
+        resp = requests.put(_resource("http://127.0.0.1:5000/{task_manager}"))
+        if resp.status_code != requests.codes.okay:
+            print("Something bad happened")
 
     # Query Job
     def get(self, wf_id):
         # Ask the scheduler how the workflow is going
-        #scheduler_query(wf_id)
-        pass
+        resp = requests.get(_resource("http://127.0.0.1:5000/{task_manager}"))
+        if resp.status_code != requests.codes.okay:
+            print("Something bad happened")
 
     # Cancel Job
     def delete(self, wf_id):
@@ -121,38 +130,20 @@ class JobActions(Resource):
         # Also need to send a request to the task manager to cancel any ongoing tasks 
         # Scheduler should return which of the tasks are still running
         pass
+        
+    ## Pause Job
+    # Not currently implemented
+    #def patch(self, wf_id):
+    #    # Send a request to the scheduler to pause the workflow
+    #    # Just like with cancel we need to work this out between the TM and sched
+    #    pass
 
-    # Pause Job
-    def patch(self, wf_id):
-        # Send a request to the scheduler to pause the workflow
-        # Just like with cancel we need to work this out between the TM and sched
-        pass
-
-
-@celery.task()
-def start_wf(wf_id):
-    # Send a request to the scheduler 
-    pass
-
-@celery.task()
-def query(wf_id):
-#    # Check with the scheduler on the progress of the workflow
-    pass
-
-@celery.task()
-def cancel(wf_id):
-#    # Cancel the workflow with the scheduler
-    pass
-
-@celery.task()
-def pause(wf_id):
-    pass
 
 api.add_resource(JobsList, '/bee_wfm/v1/jobs/')
 api.add_resource(JobSubmit, '/bee_wfm/v1/jobs/submit/<int:wf_id>')
 api.add_resource(JobActions, '/bee_wfm/v1/jobs/<int:wf_id>')
 
 if __name__ == '__main__':
-    flask_app.run(debug=True)
+    flask_app.run(debug=True, port='5000')
 
 
--- beeflow/task_manager/task_manager.py
+++ beeflow/task_manager/task_manager.py
@@ -1,12 +1,14 @@
-"""Task Manager submits & manages tasks and communicates with Work Flow Manager.
+"""Task Manager submits & manages tasks from Work Flow Manager.
 
-Submits and manages tasks & communicates status to the Work Flow Manager, through RESTful API.
-For now submit jobs serially.
+Submits, cancels and monitors states of tasks.
+Communicates status to the Work Flow Manager, through RESTful API.
 """
 import time
+import os
+import json
 
 from flask import Flask
-from flask_restful import Resource, Api, reqprse, fields
+from flask_restful import Resource, Api, reqparse, fields
 
 from beeflow.common.data.wf_data import Task
 from beeflow.common.worker.worker_interface import WorkerInterface
@@ -14,6 +16,9 @@ from beeflow.common.worker.slurm_worker import SlurmWorker
 
 flask_app = Flask(__name__)
 api = Api(flask_app)
+
+submit_queue = []  # tasks ready to be submitted
+job_queue = []  # jobs that are being monitored
 
 class TaskSubmit(Resource):
     def __init(self):
@@ -26,13 +31,6 @@ class TaskSubmit(Resource):
         data = self.reqparse.parse_args()
         task = data['task']
 
-        # If there's no file 
-        if task == "":
-            return no_file_resp
-        else:   
-            filename = "task"
-            task.save(os.path.join(flask_app.config['UPLOAD_FOLDER'], filename))
-
 
 class TaskActions(Resource):
     def __init(self):
@@ -43,47 +41,129 @@ class TaskActions(Resource):
         def post(self):
             pass
 
-        def 
+
+WORKER = WorkerInterface(SlurmWorker)
+
+def read_task(json_file):
+    """Get task from json file, will come from WFM."""
+    with open(json_file) as json_f:
+        sent_task = json.load(json_f)
+        os.remove(json_file)
+        task_cmd = sent_task['command'].split(',')
+        # fix TODO submit does not use hints and subworkflow
+        # fix TODO task_id should be part of task will it be?
+        task = Task(name=sent_task['name'],
+                    command=task_cmd,
+                    hints=None,
+                    subworkflow=None,
+                    inputs=sent_task['inputs'],
+                    outputs=sent_task['outputs'])
+        task_id = sent_task['task_id']
+        return task, task_id
 
 
-def main():
-    """Mock Task Manager populates a Task dictionary to use to submit tasks."""
-    worker = WorkerInterface(SlurmWorker)
-
-    # no restful api now, so populate Task manually using by_hand example from Al
-    tasks = []
-    t_input = 'grep.in'
-    t_output = 'grep.out'
-    grep_string = 'database'
-    tasks.append(
-        Task('GREP', command=['grep', '-i', grep_string, t_input, '>', t_output],
-             hints=None, subworkflow=None, inputs={t_input}, outputs={t_output}))
-
-    t_input = 'grep.out'
-    t_output = 'wc.out'
-    tasks.append(
-        Task('WC', command=['wc', '-l', t_input, '>', t_output],
-             hints=None, subworkflow=None, inputs={t_input}, outputs={t_output}))
-
-    # Will eventually be a server that uses RESTful API's
-    # for now  loop until tasks defined above are deleted
-    # This will be replaced by a queue of tasks associated with jobs
-    # so that we can run them in parallel.
-    print('')
-    while tasks:
-        task = tasks.pop(0)
-        job_info = worker.submit_task(task)
-        job_state = job_info[1]
-        job_id = job_info[0]
-        print('Task: ', task.name, '  Job: ', job_id, 'submitted, ', job_state)
-
-        while job_state != 'COMPLETED':
-            # check for state change every 5 seconds FIX THIS
-            time.sleep(5)
-            job_info = worker.query_job(job_id)
-            if job_info[1] != job_state:
-                job_state = job_info[1]
-                print('   ', job_id, 'changed state: ', job_state)
+def receive_task(json_file, submit_queue):
+    """Recieve task from WFM, place it in queue to submit."""
+    # for now read it from a file TODO function REST event to submit task
+    if os.path.exists(json_file):
+        task, task_id = read_task(json_file)
+        submit_queue.append({task_id: task})
 
 
-main()
+def cancel_task(json_file, job_queue):
+    """Received from WFM to cancel job, add to queue to monitor state."""
+    # for now read it from a file TODO function for REST event to cancel task
+    if os.path.exists(json_file):
+        with open(json_file) as json_f:
+            cancel_t = json.load(json_f)
+            os.remove(json_file)
+            success, job_state = WORKER.cancel_job(cancel_t["job_id"])
+        if success == 1:
+            job_queue.append({cancel_t['task_id']:
+                             {'name': cancel_t['name'],
+                              'job_id': cancel_t['job_id'],
+                              'job_state': job_state}})
+        else:
+            # fix TODO prints should send event to REST for state change
+            print('Cancel failed:',
+                  cancel_t['task_id'], cancel_t['name'], job_state)
+
+
+def update_job_queue(job_queue):
+    """Check and update states of jobs in queue, remove completed jobs."""
+    for job in job_queue:
+        task_id = list(job)[0]
+        current_task = job[task_id]
+        job_id = current_task['job_id']
+        state = WORKER.query_job(job_id)
+        if state[0] == 1:
+            job_state = state[1]
+        else:
+            job_state = 'ZOMBIE'
+        if job_state != current_task['job_state']:
+            current_task['job_state'] = job_state
+            # fix TODO new state event to WFM here
+            print('Task changed state:', task_id, current_task['name'],
+                  job_id, job_state)
+        # fix TODO needs to be an abstract state see wiki for our TM states
+        if job_state in ('COMPLETED', 'CANCELLED', 'ZOMBIE'):
+            # fix TODO create event Send job info to WFM
+            print('Job done:', task_id, current_task['name'], job_id,
+                  job_state)
+            job_queue.remove(job)
+
+
+#def main():
+#    """Task Manager submits, and cancels tasks and monitors associated jobs."""
+#    # fix TODO work with REST interface
+#    # for now: a timed loop this is just so I don't leave it running
+#    timer = 120
+#    start = time.time()
+
+    
+    ## receive task from WFM, TODO replace REST event, adds to queue
+    #receive_task('sent_task.json', submit_queue)
+
+    ## cancel job from WFM
+    ## fix TODO replace with REST event, needs task_id, task_name & job_id
+    #cancel_task('cancel.json', job_queue)
+
+   # def check_queue():
+   #     while len(submit_queue) >= 1:
+   #         task_dict = submit_queue.pop(0)
+   #         task_id = list(task_dict)[0]
+   #         task = task_dict.get(task_id)
+   #         job_id, job_state = WORKER.submit_task(task)
+
+   #         # fix TODO prints will become message sends to WFM
+   #         if job_id == -1:
+   #             error = job_state
+   #             job_state = 'SUBMIT_FAIL'
+   #             # send task failed message to WFM
+   #             print('Task failed:', task_id, task.name, job_state, error)
+   #         else:
+   #             # place job in queue to monitor and send intial state to WFM)
+   #             job_queue.append({task_id: {'name': task.name,
+   #                                         'job_id': job_id,
+   #                                         'job_state': job_state}})
+   #             print('Submitted: ', task_id, task.name,
+   #                   'Job:', job_id, job_state)
+
+   #     update_job_queue(job_queue)
+   #     time.sleep(5)  # checking state of jobs every loop so pause
+   #     # Timer is temporary just to insure task manager stops TODO
+   #     if time.time() > start + timer:
+   #         if len(job_queue) == 0 & len(submit_queue) == 0:
+   #             print('Task Manager Shut Down')
+   #             break
+   #         print('Task Manager time up, but there are still active tasks.')
+   #         time.sleep(10)
+
+
+#main()
+
+api.add_resource(TaskSubmit, '/bee_wfm/v1/task/submit/<int:wf_id>')
+api.add_resource(TaskActions, '/bee_wfm/v1/task/<int:wf_id>')
+
+if __name__ == '__main__':
+        flask_app.run(debug=True, port='5050')
