45e173e783e7d0f9b5cf25b71df174ca189e4cc6
Tim Randles <trandles@lanl.gov>
2020-04-03 13:04:40 -0600

    Use user's bee.conf to configure port values for workflow manager and task manager. Completes issue #135

--- beeflow/client/client.py
+++ beeflow/client/client.py
@@ -5,6 +5,9 @@ import logging
 import requests
 from errors import ApiError
 from pathlib import Path
+from beeflow.common.config.config_driver import BeeConfig
+
+bc = BeeConfig()
 
 logging.basicConfig(level=logging.WARNING)
 
@@ -12,7 +15,12 @@ workflow_manager = 'bee_wfm/v1/jobs'
 
 # Returns the url to the resource
 def _url():
-    return f'http://127.0.0.1:5000/{workflow_manager}/'
+    if bc.userconfig.has_section('workflow_manager'):
+        wfm_listen_port = bc.userconfig['workflow_manager'].get('listen_port','5000')
+    else:
+        print("[workflow_manager] section not found in configuration file. Default port 5000 will be used.")
+        wfm_listen_port = '5000'
+    return f'http://127.0.0.1:{wfm_listen_port}/{workflow_manager}/'
 
 def _resource(tag=""): 
     return _url() + str(tag)
