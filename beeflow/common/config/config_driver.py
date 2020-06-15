903b13efedac90e62837af116b5d1b2766d9d156
Quincy Wofford <qwofford@lanl.gov>
2020-06-15 13:00:06 -0600

    pylama pre-commit check succeeded! fixed pylama compliance, shoiudl commit now

--- beeflow/common/config/config_driver.py
+++ beeflow/common/config/config_driver.py
@@ -40,7 +40,7 @@ class BeeConfig:
             self.userconfig_file = os.path.expanduser('~/Library/Application Support/beeflow/bee.conf')
         elif system == "Windows":
             self.sysconfig_file = ''
-            self.userconfig_file = os.path.expandvars(r'%APPDATA%\beeflow\bee.conf')
+            self.userconfig_file = os.path.expandvars(r'%APPDATA%\beeflow\bee.conf') 
 
         try:
             with open(self.sysconfig_file) as sysconf_file:
