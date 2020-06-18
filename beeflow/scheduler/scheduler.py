0c320d0a9b7e84fc606f2551f163fe75dd6e95bd
Jake Tronge <jtronge3@gmail.com>
2020-06-18 13:49:04 -0400

    Add simple FCFS algorithm with tests

--- beeflow/scheduler/scheduler.py
+++ beeflow/scheduler/scheduler.py
@@ -1,74 +1,206 @@
 #!/usr/bin/env python3
 import os
 import sys
+import math
 
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 from flask_restful import Resource, Api, reqparse
 
 flask_app = Flask(__name__)
 api = Api(flask_app)
 
-class WorkflowManager(Resource):
+# TODO: Put this information into a database
+workflows = {}
+allocations = {}
+partitions = {}
+
+class WorkflowHandler(Resource):
+    """Workflow handler.
+
+    Handle the creation, deletion and updating of workflows.
+    """
+
     def post(self, name=None):
-        pass
+        """Create/send over a new workflow.
+
+        Create a new workflow.
+        :param name: name of the workflow
+        :type name: str
+        """
+        data = request.get_json()
+        if data is not None and 'name' in data:
+            name = data['name']
+            workflows[name] = data
+            url = os.path.join(request.url, name)
+            return {
+              'url': url,
+              'allocation': os.path.join(url, 'allocation'),
+            }
+        return None
 
     def get(self, name=None):
-        pass
+        """Get details about a workflow.
+
+        Get a new workflow.
+        :param name: name of the workflow
+        :type name: str
+        """
+        # TODO
 
     def put(self, name=None):
-        pass
+        """Update a workflow.
+
+        Update the workflow.
+        :param name: name of the workflow
+        :type name: str
+        """
+        # TODO
 
     def delete(self, name=None):
-        pass
+        """Delete a workflow.
+
+        Delete a workflow from the system.
+        :param name: name of the workflow
+        :type name: str
+        """
+        # TODO
 
 
-class AllocationManager(Resource):
+def schedule_best(partition_times):
+    """(Naïve implementation)
+    Return the best tuple of (partition, time) for this dictionary of
+    partitions to current scheduled times.
+
+    :param partition_times: current allocated partitions with a list
+                            of allocated times for each partition
+    """
+    best_time = math.inf
+    best_part = None
+    for partition in partition_times:
+        time = sum(partition_times[partition])
+        if time < best_time:
+            best_time = time
+            best_part = partition
+    return best_part, best_time
+
+
+# TODO: choose_algorithm() and the scheduling algorithms themselves could
+#       could be designed to be object-oriented
+def fcfs(workflow, partitions):
+    """Run a simple FCFS algorithm with the given FCFS."""
+    allocation = {
+        'status': 'COMPLETE',
+        'start_time': 0,
+    }
+    # Simple FCFS Algorithm
+    # TODO: Assumes constant time for each task
+    steps_to_partitions = {}
+    time = 0
+    total_time = 0
+    # Create a list of used times for each partition
+    partition_times = {pname: [] for pname in partitions}
+    # TODO: Set max_runtime per nodes
+    for level in workflow['tasks']:
+        max_time = 0
+        for task in level:
+            best_part, best_time = schedule_best(partition_times)
+            steps_to_partitions[task['name']] = {
+                'partition': best_part,
+                'time': best_time,
+            }
+            partition_times[best_part].append(task['runtime'])
+            if task['runtime'] > max_time:
+                max_time = task['runtime']
+        time += max_time
+        total_time += max_time
+        # "Level off" the partition_times to match the maximum running times
+        for partition in partition_times:
+            t = sum(partition_times[partition])
+            if t < total_time:
+                partition_times[partition].append(total_time - t)
+    allocation['steps_to_partition'] = steps_to_partitions
+    # TODO
+    return allocation
+
+def choose_algorithm():
+    """Choose the scheduling algorithm to run."""
+    # TODO: Choose the specific scheduling algorithm to run using known
+    #       variables
+    return fcfs
+
+
+class AllocationHandler(Resource):
+    """Allocation handler - handles main scheduling algorithm."""
     def post(self, name):
         """Start the scheduling algorithm."""
-        pass
+        print(workflows)
+        # TODO
+        return 'initiating scheduling process'
 
     def get(self, name):
         """
         Get the allocation that has been calculated or return current status.
         """
-        pass
+        algorithm = choose_algorithm()
+        return algorithm(workflows[name], partitions)
 
-
-class PartitionManager(Resource):
+class PartitionHandler(Resource):
+    """Partition Handler"""
     def post(self, name=None):
-        pass
+        """Create a new partition.
+
+        :param name: name of the parition
+        """
+        data = request.get_json()
+        if data is not None and (name is not None or 'name' in data):
+            if name is None:
+                name = data['name']
+            partitions[name] = data
+            # Setup allocation
+            allocations[name] = []
+            return {'url': os.path.join(request.url, name)}
+        # TODO
+        return None
 
     def get(self, name=None):
-        pass
+        """Get a new node."""
+        # TODO
 
     def put(self, name=None):
-        pass
+        """Update a node."""
+        # TODO
 
     def delete(self, name=None):
-        pass
+        """Delete a node."""
+        # TODO
 
 
-class NodeManager(Resource):
+class NodeHandler(Resource):
+    """Node Handler"""
     def post(self, node_id=None):
-        pass
+        """Create a new node."""
+        # TODO
 
     def get(self, node_id=None):
-        pass
+        """Get information about a node."""
+        # TODO
 
     def put(self, node_id=None):
-        pass
+        """Update a node."""
+        # TODO
 
     def delete(self, node_id=None):
-        pass
+        """Delete a node."""
+        # TODO
 
 
-api.add_resource(WorkflowManager, '/bee_sched/v1/workflows/',
+api.add_resource(WorkflowHandler, '/bee_sched/v1/workflows/',
                  '/bee_sched/v1/workflows/<string:name>')
-api.add_resource(AllocationManager,
+api.add_resource(AllocationHandler,
                  '/bee_sched/v1/workflows/<string:name>/allocation')
-api.add_resource(PartitionManager, '/bee_sched/v1/partitions/',
+api.add_resource(PartitionHandler, '/bee_sched/v1/partitions/',
                  '/bee_sched/v1/partitions/<string:name>')
-api.add_resource(NodeManager, '/bee_sched/v1/nodes/',
+api.add_resource(NodeHandler, '/bee_sched/v1/nodes/',
                  '/bee_sched/v1/nodes/<int:node_id>')
 
 if __name__ == '__main__':
---
+++ beeflow/scheduler/test_allocation.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python3
+import allocation
+
+# TODO: Perhaps structure these tests into classes based on the
+#       feature being tested.
+
+def test_partition_insert_empty_0():
+    """Test inserting into a empty partition."""
+    partition = allocation.Partition(name='test-partition-0')
+    task = allocation.Task(name='task-0', runtime=2)
+
+    partition.insert(task, start_time=0)
+
+    assert len(partition.slots) == 1
+    assert not partition.slots[0].open
+    assert partition.slots[0].task.name == 'task-0'
+    assert partition.slots[0].start_time == 0
+    assert partition.slots[0].runtime == 2
+
+def test_partition_insert_empty_1():
+    """Test inserting into an empty partition at a specific time."""
+    partition = allocation.Partition(name='test-partition-0')
+    task = allocation.Task(name='task-0', runtime=2)
+
+    partition.insert(task, start_time=3)
+
+    assert len(partition.slots) == 2
+    assert partition.slots[0].open
+    assert partition.slots[0].start_time == 0
+    assert partition.slots[0].runtime == 3
+    assert not partition.slots[1].open
+    assert partition.slots[1].task.name == 'task-0'
+    assert partition.slots[1].start_time == 3
+    assert partition.slots[1].runtime == 2
+
+def test_partition_insert_non_empty_0():
+    """Test inserting into a non-empty partition."""
+    partition = allocation.Partition(name='test-partition-0')
+    task1 = allocation.Task(name='task-0', runtime=2)
+    partition.insert(task1, start_time=3)
+    task2 = allocation.Task(name='task-1', runtime=2)
+
+    partition.insert(task2)
+
+    assert len(partition.slots) == 3
+    assert not partition.slots[0].open
+    assert partition.slots[0].task.name == 'task-1'
+    assert partition.slots[0].start_time == 0
+    assert partition.slots[0].runtime == 2
+    assert partition.slots[1].open
+    assert partition.slots[1].start_time == 2
+    assert partition.slots[1].runtime == 1
+    assert not partition.slots[2].open
+    assert partition.slots[2].task.name == 'task-0'
+    assert partition.slots[2].start_time == 3
+    assert partition.slots[2].runtime == 2
+
+def test_partition_insert_non_empty_1():
+    """Test inserting into a non-empty partition with two tasks."""
+    partition = allocation.Partition(name='test-partition-0')
+    task1 = allocation.Task(name='task-0', runtime=2)
+    partition.insert(task1)
+    task2 = allocation.Task(name='task-1', runtime=2)
+    partition.insert(task2, start_time=4)
+    task3 = allocation.Task(name='task-2', runtime=2)
+
+    partition.insert(task3)
+
+    assert len(partition.slots) == 3
+    assert not partition.slots[0].open
+    assert partition.slots[0].task.name == 'task-0'
+    assert partition.slots[0].start_time == 0
+    assert partition.slots[0].runtime == 2
+    assert not partition.slots[1].open
+    assert partition.slots[1].task.name == 'task-2'
+    assert partition.slots[1].start_time == 2
+    assert partition.slots[1].runtime == 2
+    assert not partition.slots[2].open
+    assert partition.slots[2].task.name == 'task-1'
+    assert partition.slots[2].start_time == 4
+    assert partition.slots[2].runtime == 2
+
+def test_partition_insert_non_empty_2():
+    """Test inserting into a non-empty partition with a slot left over."""
+    partition = allocation.Partition(name='test-partition-0')
+    task1 = allocation.Task(name='task-0', runtime=2)
+    partition.insert(task1)
+    task2 = allocation.Task(name='task-1', runtime=2)
+    partition.insert(task2, start_time=6)
+    task3 = allocation.Task(name='task-2', runtime=2)
+
+    partition.insert(task3)
+
+    assert len(partition.slots) == 4
+    assert not partition.slots[0].open
+    assert partition.slots[0].task.name == 'task-0'
+    assert partition.slots[0].start_time == 0
+    assert partition.slots[0].runtime == 2
+    assert not partition.slots[1].open
+    assert partition.slots[1].task.name == 'task-2'
+    assert partition.slots[1].start_time == 2
+    assert partition.slots[1].runtime == 2
+    assert partition.slots[2].open
+    assert partition.slots[2].start_time == 4
+    assert partition.slots[2].runtime == 2
+    assert not partition.slots[3].open
+    assert partition.slots[3].task.name == 'task-1'
+    assert partition.slots[3].start_time == 6
+    assert partition.slots[3].runtime == 2
+
+
+def test_partition_fit_empty_0():
+    """Test method fit() on an empty partition."""
+    partition = allocation.Partition(name='test-partition-2')
+    task = allocation.Task(name='task', runtime=44)
+
+    t = partition.fit(task)
+
+    assert t == 0
+
+def test_partition_fit_empty_1():
+    """Test method fit() on an empty partition with a start time."""
+    partition = allocation.Partition(name='test-partition-2')
+    task = allocation.Task(name='task', runtime=44)
+
+    t = partition.fit(task, start_time=1)
+
+    assert t == 1
+
+def test_partition_fit_non_empty_0():
+    """Test method fit() on a non-empty partition."""
+    partition = allocation.Partition(name='test-partition-2')
+    task1 = allocation.Task(name='task-0', runtime=44)
+    partition.insert(task1, start_time=2)
+    task2 = allocation.Task(name='task-1', runtime=2)
+
+    t = partition.fit(task2)
+
+    assert t == 0
+
+def test_partition_fit_non_empty_1():
+    """Test method fit on a non-empty partition with a small timeslot."""
+    partition = allocation.Partition(name='test-partition-2')
+    task1 = allocation.Task(name='task-0', runtime=44)
+    partition.insert(task1, start_time=2)
+    task2 = allocation.Task(name='task-1', runtime=60)
+
+    t = partition.fit(task2)
+
+    assert t == 46
+
+
+def test_fcfs_one_task_one_partition():
+    """Test the fcfs algorithm with a single task and a single partition."""
+    task = allocation.Task(name='task-0', runtime=10)
+    workflow = allocation.Workflow(name='workflow-0')
+    workflow.insert(level=0, task=task)
+    cluster = allocation.Cluster(name='cluster-0')
+    partition = allocation.Partition(name='partition-0')
+    cluster.insert_partition(partition)
+
+    provision = allocation.fcfs(workflow, [cluster], 0)
+
+    assert len(provision) == 1
+    assert provision['task-0'].partition.name == 'partition-0'
+    assert provision['task-0'].start_time == 0
+    assert provision['task-0'].task.name == 'task-0'
+    assert provision['task-0'].task.runtime == 10
+
+def test_fcfs_two_tasks_one_partition():
+    """Test the fcfs algorithm with two tasks and a single partition."""
+    task1 = allocation.Task(name='task-0', runtime=10)
+    task2 = allocation.Task(name='task-1', runtime=10)
+    workflow = allocation.Workflow(name='workflow-0')
+    workflow.insert(level=0, task=task1)
+    workflow.insert(level=1, task=task2)
+    cluster = allocation.Cluster(name='cluster-0')
+    partition = allocation.Partition(name='partition-0')
+    cluster.insert_partition(partition)
+
+    provision = allocation.fcfs(workflow, [cluster], 0)
+
+    assert len(provision) == 2
+    assert provision['task-0'].partition.name == 'partition-0'
+    assert provision['task-0'].start_time == 0
+    assert provision['task-0'].task.name == 'task-0'
+    assert provision['task-1'].partition.name == 'partition-0'
+    assert provision['task-1'].start_time == 10 
+    assert provision['task-1'].task.name == 'task-1'
