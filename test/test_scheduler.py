5ea9affb4c4d256339976c6f3b3ab9185f85cd2b
Jake Tronge <jtronge3@gmail.com>
2020-07-08 16:26:07 -0400

    Refactor backfill code, add test cases

--- test/test_scheduler.py
+++ test/test_scheduler.py
@@ -11,6 +11,7 @@ import beeflow.scheduler.allocation as allocation
 import beeflow.scheduler.sched_types as sched_types
 # import beeflow.common.data.wf_data as wf_data
 
+
 class AbstractScheduleTestCase(abc.ABC):
     """Abstract schedule test case.
 
@@ -72,6 +73,7 @@ class AbstractScheduleTestCase(abc.ABC):
                 is None)
         # No allocations available
         assert not task1.allocations
+
 
 class TestFCFS(AbstractScheduleTestCase):
     """Test the FCFS algorithm.
@@ -177,6 +179,55 @@ class TestBackfill(AbstractScheduleTestCase):
         assert task3.allocations[0].id_ == 'resource-0'
         assert task3.allocations[0].cores == 1
         assert task3.allocations[0].start_time == current_time
+
+    @staticmethod
+    def test_schedule_four_tasks():
+        """Test scheduling four tasks.
+
+        Test scheduling four tasks.
+        """
+        requirements = {'max_runtime': 2, 'cores': 4}
+        task1 = sched_types.Task(workflow_name='workflow-0',
+                                 task_name='task-0',
+                                 requirements=requirements)
+        requirements = {'max_runtime': 2, 'cores': 8}
+        task2 = sched_types.Task(workflow_name='workflow-0',
+                                 task_name='task-1',
+                                 requirements=requirements)
+        requirements = {'max_runtime': 3, 'cores': 2}
+        # This task should not be backfilled (too much time)
+        task3 = sched_types.Task(workflow_name='workflow-0',
+                                 task_name='task-2',
+                                 requirements=requirements)
+        requirements = {'max_runtime': 1, 'cores': 2}
+        # This task should be backfilled
+        task4 = sched_types.Task(workflow_name='workflow-0',
+                                 task_name='task-3',
+                                 requirements=requirements)
+        resource1 = sched_types.Resource(id_='resource-0', cores=2)
+        resource2 = sched_types.Resource(id_='resource-1', cores=2)
+        resource3 = sched_types.Resource(id_='resource-2', cores=2)
+        resource4 = sched_types.Resource(id_='resource-3', cores=2)
+
+        tasks = [task1, task2, task3, task4]
+        resources = [resource1, resource2, resource3, resource4]
+        allocation.schedule_all(algorithms.Backfill, tasks, resources)
+
+        current_time = int(time.time())
+        assert len(task1.allocations) == 2
+        assert all(a.start_time == current_time for a in task1.allocations)
+        assert len(task2.allocations) == 4
+        start_time = current_time + task1.requirements['max_runtime']
+        assert all(a.start_time == start_time for a in task2.allocations)
+        # task3 should not have been backfilled (would have taken too
+        # much time)
+        assert len(task3.allocations) == 1
+        start_time = (current_time + task1.requirements['max_runtime']
+                      + task2.requirements['max_runtime'])
+        assert task3.allocations[0].start_time == start_time
+        # task4 should have been backfilled
+        assert len(task4.allocations) == 1
+        assert task4.allocations[0].start_time == current_time
 
     @staticmethod
     def test_schedule_six_tasks():
