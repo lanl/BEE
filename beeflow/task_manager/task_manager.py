c87693a605bbfad870a7ebc4e68ecb6580b7be24
Patricia Grubel <pagrubel@lanl.gov>
2020-08-07 14:05:04 -0600

    Correct default for ch-run options.

--- beeflow/task_manager/task_manager.py
+++ beeflow/task_manager/task_manager.py
@@ -32,7 +32,7 @@ def check_crt_config(container_runtime):
         if not bc.userconfig.has_section('charliecloud'):
             cc_opts = {'setup': 'module load charliecloud',
                        'image_mntdir': '/tmp',
-                       'chrun_opts': '--cd /home/$USER --write'
+                       'chrun_opts': '--cd /home/$USER'
 
                        }
             bc.modify_section('user', 'charliecloud', cc_opts)
