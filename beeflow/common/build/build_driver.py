30ee439404be15524a1ff36df277036f77a6f95a
Quincy Wofford <qwofford@lanl.gov>
2020-09-23 13:08:49 -0600

    abstract class methods added for CWL compliant build driver template

--- beeflow/common/build/build_driver.py
+++ beeflow/common/build/build_driver.py
@@ -7,7 +7,7 @@ class BuildDriver(ABC):
     """Driver interface between WFM and a generic build system.
 
     A driver object must implement an __init__ method that
-    requests a Runtime Environment (RTE), and a method to 
+    requests a Runtime Environment (RTE), and a method to
     return the requested RTE.
     """
 
@@ -56,3 +56,57 @@ class BuildDriver(ABC):
 
         Confirm build procedure completed successfully.
         """
+
+    @abstractmethod
+    def dockerPull(self):
+        """CWL compliant dockerPull.
+
+        CWL spec 09-23-2020: Specify a Docker image to
+        retrieve using docker pull. Can contain the immutable
+        digest to ensure an exact container is used.
+        """
+
+    @abstractmethod
+    def dockerLoad(self):
+        """CWL compliant dockerLoad.
+
+        CWL spec 09-23-2020: Specify a HTTP URL from which to
+        download a Docker image using docker load.
+        """
+
+    @abstractmethod
+    def dockerFile(self):
+        """CWL compliant dockerFile.
+
+        CWL spec 09-23-2020: Supply the contents of a Dockerfile
+        which will be built using docker build.
+        """
+
+    @abstractmethod
+    def dockerImport(self):
+        """CWL compliant dockerImport.
+
+        CWL spec 09-23-2020: Provide HTTP URL to download and
+        gunzip a Docker images using docker import.
+        """
+
+    @abstractmethod
+    def dockerImageId(self):
+        """CWL compliant dockerImageId.
+
+        CWL spec 09-23-2020: The image id that will be used for
+        docker run. May be a human-readable image name or the
+        image identifier hash. May be skipped if dockerPull is
+        specified, in which case the dockerPull image id must be
+        used.
+        """
+
+    @abstractmethod
+    def dockerOutputDirectory(self):
+        """CWL compliant dockerOutputDirectory.
+
+        CWL spec 09-23-2020: Set the designated output directory
+        to a specific location inside the Docker container.
+        """
+# Ignore snake_case requirement to enable CWL compliant names.
+# pylama:ignore=C0103
