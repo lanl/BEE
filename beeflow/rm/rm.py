e30f276490fd5ff452eb6f05625f1e6be031593c
Jake Tronge <jtronge3@gmail.com>
2020-12-21 14:06:17 -0500

    Add initial ResourceMonitor design

---
+++ beeflow/rm/rm.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Resource Monitor.
+
+"""
+
+# Load BeeConfig
+# TODO
+
+# /bee_rm/v1/nodes URL
+# TODO
+# /bee_rm/v1/partitions URL
+# TODO
+# /bee_rm/v1/reservations URL
+# TODO
+# /bee_rm/v1/qos URL
+# TODO
+
+if __name__ == '__main__'
+    # TODO
+    pass
