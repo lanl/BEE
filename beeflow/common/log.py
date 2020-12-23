177d47155ed56d5c1b80eeb3aba1b535200e47a0
Quincy Wofford <qwofford@lanl.gov>
2020-12-23 13:06:33 -0700

    don't redefine handlers for existing loggers

--- beeflow/common/log.py
+++ beeflow/common/log.py
@@ -5,6 +5,7 @@ import os
 STEP_INFO = 15
 logging.addLevelName(STEP_INFO, "STEP_INFO")
 
+
 class LogFormatter(logging.Formatter):
     """Format a string for the log file.
     
@@ -27,8 +28,8 @@ class LogFormatter(logging.Formatter):
 
     log_format = {
         "DEBUG": f"{BOLD_CYAN}%(levelname)s: %(msg)s {RESET}",
-        "STEP_INFO": "%(msg)s",
-        "INFO": "%(msg)s",
+        "STEP_INFO": "%(levelname)s: %(msg)s",
+        "INFO": "%(levelname)s: %(msg)s",
         "WARNING": f"{BOLD_YELLOW}%(levelname)s: %(msg)s{RESET}",
         "ERROR": f"{BOLD_RED}%(levelname)s: %(msg)s{RESET}",
         "CRITICAL": f"{BOLD_RED}%(levelname)s: %(msg)s{RESET}",
@@ -36,15 +37,15 @@ class LogFormatter(logging.Formatter):
 
     log_format_no_colors = {
         "DEBUG": "%(levelname)s: %(msg)s ",
-        "STEP_INFO": "%(msg)s",
-        "INFO": "%(msg)s",
+        "STEP_INFO": "%(levelname)s: %(msg)s",
+        "INFO": "%(levelname)s: %(msg)s",
         "WARNING": "%(levelname)s: %(msg)s",
         "ERROR": "%(levelname)s: %(msg)s",
         "CRITICAL": "%(levelname)s: %(msg)s",
     }
 
 
-    def __init__(self, colors=True):
+    def __init__(self, colors):
         """Initialize formatted loggers.
 
         :param colors: Format with or without ANSI Escape Code color formatting
@@ -60,6 +61,22 @@ class LogFormatter(logging.Formatter):
         """
         fmt = self.log_fmt.get(logging.getLevelName(record.levelno))
         return logging.Formatter(fmt).format(record)
+
+class BeeLogger(logging.Logger):
+    """ Extend Python logger to handle custom log category."""
+
+    def step_info(self, msg="", *args, **kwargs):
+        """ Log a messagewith severity 'STEP_INFO'.
+
+        :param msg: Message to be logged
+        :type msg: String
+        :param *args: List of non-key worded, variable length args.
+        :type *args: list
+        :param **kwargs: Key-worded, variable length args.
+        :type **kwargs: dict
+        """
+        if self.isEnabledFor(STEP_INFO):
+            self._log(STEP_INFO, msg, args, **kwargs)
 
 class LevelFilter(logging.Filter):
     """Filters the level that are to be accepted and rejected."""
@@ -80,30 +97,34 @@ class LevelFilter(logging.Filter):
             return record.levelno in self.passlevels
 
 
-def setup_logging(level="STEP_INFO"):
+def setup_logging(level="STEP_INFO", colors=True):
     """Setup logger.
 
     :param level: Level to be logged in logger.
     :type level: String
     """
+    logging.setLoggerClass(BeeLogger)
     log = logging.getLogger("bee")
-    formatter = LogFormatter()
-
-    # Intermediate step info goes to stdout
-    h1 = logging.StreamHandler(sys.stdout)
-    h1.addFilter(LevelFilter([logging.INFO, STEP_INFO], False))
-    h1.setFormatter(formatter)
-
-    # All stepinfo goes to stdout
-    h2 = logging.StreamHandler(sys.stdout)
-    h2.addFilter(LevelFilter([logging.INFO, STEP_INFO], True))
-    h2.setFormatter(formatter)
-
-    log.addHandler(h1)
-    log.addHandler(h2)
-    log.setLevel(level)
-
-    return log
+    if log.hasHandlers():
+        log.setLevel(level)
+        return log
+    else:
+        formatter = LogFormatter(colors=colors)
+    
+        # Intermediate step info goes to stdout
+        h1 = logging.StreamHandler(sys.stdout)
+        h1.addFilter(LevelFilter([logging.INFO, STEP_INFO], False))
+        h1.setFormatter(formatter)
+    
+        # All stepinfo goes to stdout
+        h2 = logging.StreamHandler(sys.stdout)
+        h2.addFilter(LevelFilter([logging.INFO, STEP_INFO], True))
+        h2.setFormatter(formatter)
+    
+        log.addHandler(h1)
+        log.addHandler(h2)
+        log.setLevel(level)
+        return log
 
 def add_log(log, logfile):
     """Set log formatter for handle and add handler to logger.
