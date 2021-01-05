d00983d16dc418838c3655ce51696ebc55dc0c46
Jake Tronge <jtronge3@gmail.com>
2021-01-05 09:42:57 -0500

    Add SubsystemInterface

---
+++ beeflow/common/subsystem/subsystem_interface.py
@@ -0,0 +1,34 @@
+"""Subsystem interface class.
+
+"""
+
+
+class SubsystemInterface:
+    """Subsystem interface.
+
+    """
+
+    def __init__(self):
+        """Subsystem interface constructor.
+
+        """
+
+    def get_nodes(self):
+        """Get node information for the subsystem.
+
+        """
+
+    def get_partitions(self):
+        """Get partition information for the subsystem.
+
+        """
+
+    def get_reservations(self):
+        """Get reservation information for the subsystem.
+
+        """
+
+    def get_qos_information(self):
+        """Get QOS information for the subsystem.
+
+        """
