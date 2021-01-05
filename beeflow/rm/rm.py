c0eeec055b787f268a549468def49fc0a56b4b89
Jake Tronge <jtronge3@gmail.com>
2021-01-05 09:00:08 -0500

    Add empty subsystem files

--- beeflow/rm/rm.py
+++ beeflow/rm/rm.py
@@ -2,21 +2,76 @@
 """Resource Monitor.
 
 """
+from flask import Flask
+from flask_restful import Resource, Api
+
+
+app = Flask(__name__)
+api = Api(app)
 
 # TODO: Should slurm interface be in the worker code?
 
 # Load BeeConfig
 # TODO
 
-# /bee_rm/v1/nodes URL
-# TODO
-# /bee_rm/v1/partitions URL
-# TODO
-# /bee_rm/v1/reservations URL
-# TODO
-# /bee_rm/v1/qos URL
-# TODO
+# Note: The Resource Monitor will not cause any state changes, but will simply
+# be able to report information about particular resources. This is why all end
+# points will only support GET requests.
 
-if __name__ == '__main__'
+# /bee_rm/v1/nodes URL
+class NodesResource(Resource):
+    """
+    """
+
+    def get(self):
+        """
+        """
+        pass
+        # TODO
+
+
+# /bee_rm/v1/partitions URL
+class PartitionsResource(Resource):
+    """
+    """
+
+    def get(self):
+        """
+        """
+        pass
+        # TODO
+
+
+# /bee_rm/v1/reservations URL
+class ReservationsResource(Resource):
+    """
+    """
+
+    def get(self):
+        """
+        """
+        pass
+        # TODO
+
+
+# /bee_rm/v1/qos URL
+class QOSResource(Resource):
+    """
+    """
+
+    def get(self):
+        """
+        """
+        pass
+        # TODO
+
+
+api.add_resource(NodesResource, '/bee_rm/v1/nodes')
+api.add_resource(PartitionsResource, '/bee_rm/v1/partitions')
+api.add_resource(ReservationsResource, '/bee_rm/v1/reservations')
+api.add_resource(QOSResource, '/bee_rm/v1/qos')
+
+if __name__ == '__main__':
+    # Running in debug mode
+    app.run(debug=True)
     # TODO
-    pass
