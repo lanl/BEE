09d13b33da469f37d1c42192cfe1bd5967c93858
Steven Anaya <sanaya@lanl.gov>
2021-05-26 12:55:15 -0600

    Parser produces clamr workflow pseudo-graph

--- src/beeflow/common/parser2/parser.py
+++ src/beeflow/common/parser2/parser.py
@@ -5,27 +5,33 @@ tools in the graph database. This parser supports a subset of the CWL v1.0 stand
 inspired by the CWL parser written by the CWL parser written by the SABER team at John Hopkins
 University (see SABER project at https://github.com/aplbrain/saber).
 """
-
+import configparser
 import sys
 import argparse
 import json
 import os
-import re
 import yaml
 import cwl_utils.parser_v1_2 as cwl_parser
 from beeflow.common.config_driver import BeeConfig
-from beeflow.common.wf_data import Input, Output, Hint, Requirement
+from beeflow.common.wf_data import (InputParameter,
+                                    OutputParameter,
+                                    StepInput,
+                                    StepOutput,
+                                    Hint,
+                                    Requirement)
 from beeflow.common.wf_interface import WorkflowInterface
 
-bc = BeeConfig()
 try:
+    bc = BeeConfig()
     wfi = WorkflowInterface(user='neo4j', bolt_port=bc.userconfig.get('graphdb', 'bolt_port'),
                             db_hostname=bc.userconfig.get('graphdb', 'hostname'),
                             password=bc.userconfig.get('graphdb', 'dbpass'))
 except KeyError:
     wfi = WorkflowInterface()
+except configparser.NoSectionError:
+    wfi = WorkflowInterface()
 
-type_table = {
+type_map = {
     'string': str,
     'int': int,
     'long': int,
@@ -63,37 +69,37 @@ class CwlParser:
             # Parse input job params into self.params
             self.parse_job(job)
 
+        def resolve_inputs(wf_inputs):
+            rv = {}
+            for k, v in wf_inputs.items():
+                print(f'inputs k: {k}   v: {v}')
+                print(f'type(v):   {type_map[v]}')
+                print(f'params[{k}]: {self.params[k]}')
+                print(f'type(params[{k}]): {type(self.params[k])}')
+                if not isinstance(self.params[k], type_map[v]):
+                    raise ValueError(f'Types of input/param do not match: {v}/{self.params[k]}')
+                rv[k] = self.params[k]
+            return rv
+
         workflow_name = os.path.basename(cwl).split(".")[0]
         # Steven had tuples, I changed to dict to mirror output of parse_job
-        workflow_inputs = {_shortname(input_.id): input_.type for input_ in self.cwl.inputs}
-        resolved_inputs = self.resolve_inputs(workflow_inputs)
+        #workflow_inputs = {_shortname(input_.id): input_.type for input_ in self.cwl.inputs}
+        workflow_inputs = {_shortname(input_.id) for input_ in self.cwl.inputs}
+        #resolved_inputs = resolve_inputs(workflow_inputs)
         print(f'self.params: {self.params}')
         print(f'workflow_inputs: {workflow_inputs}')
-        print(f'resolved_inputs: {resolved_inputs}')
-        workflow_outputs = {(_shortname(output.outputSource, True), output.type)
-                            for output in self.cwl.outputs}
+        #print(f'resolved_inputs: {resolved_inputs}')
+        #workflow_outputs = {(_shortname(output.outputSource, True), output.type)
+        #                    for output in self.cwl.outputs}
+        workflow_outputs = {_shortname(output.outputSource, True) for output in self.cwl.outputs}
         workflow_hints = self.parse_requirements(self.cwl.hints, as_hints=True)
         workflow_requirements = self.parse_requirements(self.cwl.requirements)
 
         workflow = wfi.initialize_workflow(workflow_name, workflow_inputs, workflow_outputs,
                                            workflow_requirements, workflow_hints)
-
         tasks = [self.parse_step(step) for step in self.cwl.steps]
 
         return workflow, tasks
-
-    def resolve_inputs(self, wf_inputs):
-        rv = {}
-        for k, v in wf_inputs.items():
-            types = {'int': int, 'float': float, 'string': str}
-            print(f'inputs k: {k}   v: {v}')
-            print(f'type(v):   {types[v]}')
-            print(f'params[{k}]: {self.params[k]}')
-            print(f'type(params[{k}]): {type(self.params[k])}')
-            if not isinstance(self.params[k], types[v]):
-                raise ValueError(f'Types of input/param do not match: {v}/{self.params[k]}')
-            rv[k] = self.params[k]
-        return rv
 
     def parse_step(self, step):
         """Parse a CWL step object.
@@ -104,14 +110,24 @@ class CwlParser:
         """
         step_cwl = cwl_parser.load_document(step.run)
 
-        if self.cwl.class_ != "CommandLineTool":
+        if step_cwl.class_ != "CommandLineTool":
             raise ValueError(f"{os.path.basename(step.run)} class must be CommandLineTool")
 
         step_name = os.path.basename(step_cwl.id).split(".")[0]
-        step_inputs = {_shortname(input_.id) for input_ in step_cwl.inputs}
-        step_outputs = {_shortname(output.id) for output in step_cwl.outputs}
-        workflow_hints = self.parse_requirements(step_cwl.hints, as_hints=True)
-        workflow_requirements = self.parse_requirements(step_cwl.requirements)
+        print(f'step name: {step_name}')
+        step_inputs = {_shortname(input_.source) for input_ in step.in_}
+        print(f'step inputs: {step_inputs}')
+        step_outputs = {f"{step_name}/{_shortname(output.id)}" for output in step_cwl.outputs}
+        print(f'step outputs: {step_outputs}')
+        step_hints = self.parse_requirements(step.hints, as_hints=True).union(
+            self.parse_requirements(step_cwl.hints, as_hints=True)
+        )
+        step_requirements = self.parse_requirements(step.requirements).union(
+            self.parse_requirements(step_cwl.requirements)
+        )
+
+        return wfi.add_task(step_name, hints=step_hints, requirements=step_requirements,
+                            inputs=step_inputs, outputs=step_outputs)
 
     def parse_job(self, job):
         """Parse a CWL input job file.
@@ -135,16 +151,19 @@ class CwlParser:
             if not isinstance(v, (str, int, float)):
                 raise ValueError(f'Invalid input job parameter type: {type(v)}')
 
-    def parse_requirements(self, requirements, as_hints=False):
+    @staticmethod
+    def parse_requirements(requirements, as_hints=False):
         """Parse CWL hints/requirements.
 
         :param requirements: the CWL requirements dictionary
         :type requirements: list of ordereddict
         :param as_hints: parse as hints instead of requirements
         :type as_hints: bool
-        :rtype: list of Hint or list of Requirement or None
+        :rtype: set of Hint or set of Requirement or None
         """
         reqs = set()
+        if not requirements:
+            return reqs
         if as_hints:
             for req in requirements:
                 pairs = ((k, v) for k, v in req.items() if k != "class")
@@ -174,8 +193,9 @@ def _shortname(uri, output_source=False):
     if output_source:
         output = uri.split("#")[-1]
         return "/".join(output.split("/")[1:])
-    else:
-        return uri.split("#")[-1]
+
+    return uri.split("#")[-1]
+
 
 def parse_args(args=sys.argv[1:]):
     """Parse arguments."""
@@ -186,10 +206,11 @@ def parse_args(args=sys.argv[1:]):
 
     return parser.parse_args(args)
 
+
 def main():
-    wf = CwlParser()
+    parser = CwlParser()
     args = parse_args()
-    wf.parse_workflow(args.wf_file, args.wf_inputs)
+    workflow, tasks = parser.parse_workflow(args.wf_file, args.wf_inputs)
 
 
 if __name__ == "__main__":
