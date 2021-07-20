4e562f742b3dd3cf2e5dbae9c167806a326a5e25
Jake Tronge <jtronge3@gmail.com>
2021-07-20 13:51:14 -0400

    Remove old TODOs

--- src/beeflow/common/worker/simple_worker.py
+++ src/beeflow/common/worker/simple_worker.py
@@ -2,9 +2,11 @@
 """Simple Worker class for launching tasks on a system with no workload manager."""
 
 import subprocess
-import beeflow.common.worker.worker as worker
 
+import beeflow.common.worker.worker as worker
 from beeflow.common.crt_interface import ContainerRuntimeInterface
+from beeflow.cli import log
+import beeflow.common.log as bee_logging
 
 # Import all implemented container runtime drivers now
 # No error if they don't exist
@@ -21,10 +23,17 @@ except ModuleNotFoundError:
 class SimpleWorker(worker.Worker):
     """The Worker interface for no workload manager."""
 
-    def __init__(self, **kwargs):
+    def __init__(self, container_runtime, **kwargs):
         """Simple worker class."""
         self.tasks = {}
-        # Use Charliecloud for now
+        # TODO: This code should be put into the driver code itself
+        crt_class = None
+        if container_runtime == 'Charliecloud':
+            crt_class = CharliecloudDriver
+        elif container_runtime == 'Singularity':
+            crt_class = SingularityDriver
+        if crt_class is None:
+            log.warning("No container runtime specified in config, proceeding with caution.")
         self.crt = ContainerRuntimeInterface(CharliecloudDriver)
 
     def submit_task(self, task):
@@ -33,15 +42,9 @@ class SimpleWorker(worker.Worker):
         :param task: instance of Task
         :rtype tuple (int, string)
         """
-        # TODO: Run the script
-        # TODO: Container runtimes
-        # TODO: MPI jobs
-        # proc = subprocess.Popen(task.command.split())
         script = self.crt.script_text(task)
         print(script)
-        # subprocess.Popen(['/bin/sh', task.command])
         self.tasks[task.id] = subprocess.Popen(['/bin/sh', '-c', script])
-        # self.tasks[task.id] = proc
         return (task.id, 'PENDING')
 
     def cancel_task(self, job_id):
@@ -62,7 +65,7 @@ class SimpleWorker(worker.Worker):
         :rtype string
         """
         rc = self.tasks[job_id].poll()
-        # This assumes a standard returncode
+        # XXX: This assumes a standard returncode
         if rc is None:
             return 'RUNNING'
         elif rc == 0:
