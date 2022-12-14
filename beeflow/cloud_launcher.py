b76527f69ca09f7b3b0b159bc11c4d966e24e536
Andres Quan <aquan@lanl.gov>
2022-12-14 08:51:54 -0700

    Pylama issues resolved for cloud_launcher

--- beeflow/cloud_launcher.py
+++ beeflow/cloud_launcher.py
@@ -205,8 +205,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-# Ignore R1732: Using a 'with' statement for resource allocation doesn't really apply here. We'd
-#               need to completely restructure the code to use it, but it's something to think
-#               about later on.
-# Ignore W0511: This TODO needs to be addressed later on, perhaps with a restructure of this code.
-# pylama:ignore=R1732,W0511
