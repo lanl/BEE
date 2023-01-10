f1e1b39dec01f4a034330429ccba8a78d52e3e26
Patricia Grubel <pagrubel@lanl.gov>
2023-01-10 13:28:00 -0700

    Pylama fix for machine learning examples

--- beeflow/data/cwl/cwl_validation/ml-workflow/example-1/add_step1.py
+++ beeflow/data/cwl/cwl_validation/ml-workflow/example-1/add_step1.py
@@ -1,13 +1,15 @@
-import click
+"""Add Step."""
 import json
+import click
 
 
 @click.command()
 @click.argument('x', type=int)
 @click.argument('y', type=int)
 def add(x, y):
-    click.echo(json.dumps({'answer': x+y}))
+    """Add."""
+    click.echo(json.dumps({'answer': x + y}))
 
 
 if __name__ == '__main__':
-    add()
+    add(x=1, y=2)
--- beeflow/data/cwl/cwl_validation/ml-workflow/example-1/multiply_step2.py
+++ beeflow/data/cwl/cwl_validation/ml-workflow/example-1/multiply_step2.py
@@ -1,13 +1,16 @@
-import click
+"""Multiply Step."""
+
 import json
+import click
 
 
 @click.command()
 @click.argument('x', type=int)
 @click.argument('y', type=int)
 def multiply(x, y):
-    click.echo(json.dumps({'answer': x*y}))
+    """Multiply."""
+    click.echo(json.dumps({'answer': x * y}))
 
 
 if __name__ == '__main__':
-    multiply()
+    multiply(x=2, y=10)
