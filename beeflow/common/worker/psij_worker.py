480293520800a0b1e581ef4c7937325c5924884b
Andres Quan <aquan@lanl.gov>
2023-06-11 12:13:54 -0600

    Some more runtime bug fixes.

--- beeflow/common/worker/psij_worker.py
+++ beeflow/common/worker/psij_worker.py
@@ -9,7 +9,7 @@ import urllib
 import getpass
 import requests_unixsocket
 import requests
-from psij import Job, JobExecutor, JobSpec
+from psij import Job, JobExecutor, JobSpec, JobAttributes, ResourceSpec
 
 from beeflow.common import log as bee_logging
 from beeflow.common.worker.worker import (Worker, WorkerError)
@@ -20,11 +20,14 @@ log = bee_logging.setup(__name__)
 
 class PSIJWorker(Worker):
     """Main Psij worker class. """
-    def __init__(self, **kwargs):
+    def __init__(self, default_account='', default_time_limit='', default_partition='', **kwargs):
         """ Construct the psij worker """
         super().__init__(**kwargs)
-        self.ex = JobExecutor.get_instance("local")
-        jobs = {}
+        self.ex = JobExecutor.get_instance("slurm")
+        self.jobs = {}
+        self.default_account = default_account
+        self.default_time_limit = default_time_limit
+        self.default_partition = default_partition
         
     def write_script(self, task):
         """Build task script; returns filename of script."""
@@ -53,18 +56,6 @@ class PSIJWorker(Worker):
         pass
 
     def submit_task(self, task):
-        #Get the executable for the task
-
-        script = self.build_text(task)
-        script_path = os.path.join(self.task_save_path(task), f'{task.name}-{task.id}.sh')
-        with open(script_path, 'w', encoding='UTF-8') as fp:
-            fp.write(script)
-        """
-        with subprocess.Popen(['/bin/sh', script_path]) as taskid:
-            self.tasks[task.id] = taskid
-        return (task.id, 'PENDING')
-        """
-
         #Get the requirements for the task
         nodes = task.get_requirement('beeflow:MPIRequirement', 'nodes', default=1)
         ntasks = task.get_requirement('beeflow:MPIRequirement', 'ntasks', default=1)
@@ -79,7 +70,7 @@ class PSIJWorker(Worker):
                                        default=self.default_account)
 
         #Apply all of the information gathered to the job spec and submit
-        js = JobSpec(executable='/bin/sh', arguments=[write_script(task)])
+        js = JobSpec(executable='/bin/sh', arguments=task.command)
         job_attributes = JobAttributes(queue_name=partition,duration=time_limit,project_name=account)
         js.resource_spec = ResourceSpec(node_count=nodes, processes_per_node=(ntasks / nodes), process_count=ntasks)
         js.attributes = job_attributes
@@ -87,7 +78,7 @@ class PSIJWorker(Worker):
         self.ex.submit(job)
         self.jobs[job.native_id] = job
         #TODO translate psi-j job status to beeflow job status
-        beeflow_state = translate_state(job.status.state)
+        beeflow_state = self.translate_state(job.status.state)
         return job.native_id,beeflow_state
 
     def cancel_task(self, job_id):
@@ -96,5 +87,5 @@ class PSIJWorker(Worker):
         return job_state
 
     def query_task(self, job_id):
-        return translate_state(self.jobs[job_id].status.state)
+        return self.translate_state(self.jobs[job_id].status.state)
 
