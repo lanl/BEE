0dbfa0799af8c1725d55766ae7f339dbeeb49f8d
Jake Tronge <jtronge@lanl.gov>
2024-03-18 13:27:29 -0600

    Make initial updates for using neo4j 5.x
    This updates the neo4j container management code and driver version.
    The gdb code has also been updated to work with the new driver version,
    which made breaking changes to how transaction functions are handled.
    
    No changes have been made to the cypher queries yet, although they
    seemed to work for the cat-grep-tar workflow.

--- beeflow/wf_manager/common/dep_manager.py
+++ beeflow/wf_manager/common/dep_manager.py
@@ -95,11 +95,11 @@ def setup_gdb_configs(mount_dir, bolt_port, http_port, https_port):
     with open(gdb_configfile, "rt", encoding="utf8") as cfile:
         data = cfile.read()
 
-    bolt_config = r'#(dbms.connector.bolt.listen_address=):[0-9]*'
+    bolt_config = r'#(server.bolt.listen_address=):[0-9]*'
     data = re.sub(bolt_config, rf'\1:{bolt_port}', data)
-    http_config = r'#(dbms.connector.http.listen_address=):[0-9]*'
+    http_config = r'#(server.http.listen_address=):[0-9]*'
     data = re.sub(http_config, rf'\1:{http_port}', data)
-    https_config = r'#(dbms.connector.https.listen_address=):[0-9]*'
+    https_config = r'#(server.https.listen_address=):[0-9]*'
     data = re.sub(https_config, rf'\1:{https_port}', data)
     with open(gdb_configfile, "wt", encoding="utf8") as cfile:
         cfile.write(data)
@@ -155,7 +155,7 @@ def start_gdb(mount_dir, bolt_port, http_port, https_port, reexecute=False):
     container_path = get_container_dir()
     if not reexecute:
         try:
-            command = ['neo4j-admin', 'set-initial-password', str(db_password)]
+            command = ['neo4j-admin', 'dbms', 'set-initial-password', str(db_password)]
             subprocess.run([
                 "ch-run",
                 "--set-env=" + container_path + "/ch/environment",
@@ -187,7 +187,8 @@ def start_gdb(mount_dir, bolt_port, http_port, https_port, reexecute=False):
                                "--", *command
                                ], stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
             output = proc.stdout.read().decode('utf-8')
-            pid = re.search(r'pid ([0-9]*)', output).group(1)
+            dep_log.info(output)
+            pid = re.search(r'\(pid:([0-9]*)\)', output).group(1)
             return pid
     except FileNotFoundError:
         dep_log.error("Neo4j failed to start.")
