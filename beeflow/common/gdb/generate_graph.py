c8a976289f1d2c01e58168c1c165ea7fe2772c11
leahh <leahh@lanl.gov>
2024-09-12 11:11:00 -0600

    added hint and requirement nodes to generate_graph

--- beeflow/common/gdb/generate_graph.py
+++ beeflow/common/gdb/generate_graph.py
@@ -10,8 +10,13 @@ bee_workdir = paths.workdir()
 dags_dir = os.path.join(bee_workdir, 'dags')
 graphmls_dir = dags_dir + "/graphmls"
 
+
 def generate_viz(wf_id):
-    """Function to make the png."""
+    """
+    Generate a PNG of a workflow graph from a GraphML file.
+    This function reads a GraphML file, processes its nodes and edges into a Graphviz
+    directed graph, and saves the rendered PNG to the output path.
+    """
     short_id = wf_id[:6]
     graphml_path = graphmls_dir + "/" + short_id + ".graphml"
     output_path = dags_dir + "/" + short_id
@@ -29,7 +34,7 @@ def generate_viz(wf_id):
             node_label = "Workflow"
             color = 'steelblue'
         if label == ":Output":
-            node_label = node[1].get('glob', node_id)
+            node_label = node[1].get('value', node_id)
             color = 'mediumseagreen'
         if label == ":Metadata":
             node_label = node[1].get('state', node_id)
@@ -40,6 +45,12 @@ def generate_viz(wf_id):
         if label == ":Input":
             node_label = node[1].get('source', node_id)
             color = 'sandybrown'
+        if label == ":Hint":
+            node_label = node[1].get('class', node_id)
+            color = 'plum'
+        if label == ":Requirement":
+            node_label = node[1].get('class', node_id)
+            color = 'lightpink1'
 
         dot.node(node_id, label=node_label, style='filled', fillcolor=color)
 
