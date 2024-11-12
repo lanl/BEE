d0e68fbfee610277687a6510a8bacf0a8d050c73
Andres Quan <aquan9@users.noreply.github.com>
2024-11-12 16:04:46 -0700

    Use random pass for neo4j (#948)
    * Use random pass for neo4j

--- beeflow/common/config_driver.py
+++ beeflow/common/config_driver.py
@@ -3,6 +3,7 @@
 from configparser import ConfigParser
 import getpass
 import os
+import base64
 import platform
 import random
 import shutil
@@ -305,7 +306,14 @@ VALIDATOR.option('charliecloud', 'setup', default='',
 VALIDATOR.section('graphdb', info='Main graph database configuration section.')
 VALIDATOR.option('graphdb', 'hostname', default='localhost',
                  info='hostname of database')
-VALIDATOR.option('graphdb', 'dbpass', default='password', info='password for database')
+
+
+# Generate random initial password for neo4j
+random_bytes = os.urandom(32)
+random_pass = base64.b64encode(random_bytes).decode('utf-8')
+
+VALIDATOR.option('graphdb', 'dbpass', default=random_pass, info='password for database')
+
 VALIDATOR.option('graphdb', 'gdb_image_mntdir', default=join_path('/tmp', USER),
                  info='graph database image mount directory', validator=validation.make_dir)
 VALIDATOR.option('graphdb', 'sleep_time', validator=int, default=1,
