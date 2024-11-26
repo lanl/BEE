414bcab415b6680b2177bed1dd4f5e81005214eb
Leah Howell <77810960+Leahh02@users.noreply.github.com>
2024-11-26 15:51:27 -0700

    Issue943/pull deps neo4j (#945)
    Enable beeflow core pull-deps to use a Dockerfile to build the needed neo4j container and add the apoc file
    
    * create  AlterConfig class and config_uitls
    
    * Add path of newly built/pulled containers to config
    
    * enable pull-deps to run when there isn't an existing config
    
    * remove some of the excess ouptput when creating a config
    
    * update tests to use the AlterConfig code.
    
    * add to pull-deps documentation and add explanation for how to use AlterConfig.
    
    * add config utils tests
    
    * change name of config sections to names from the bee config
    
    * add a config pytest fixture and change validator class name
    
    
    ---------
    
    Co-authored-by: leahh <leahh@lanl.gov>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- ci/bee_install.sh
+++ ci/bee_install.sh
@@ -8,10 +8,9 @@ printf "\n\n"
 printf "**Setting up BEE containers**\n"
 printf "\n\n"
 mkdir -p $HOME/img
-# Pull the Neo4j container
-chmod +x ./ci/install_apoc_docker
-ch-image build -t apoc_neo4j -f ./ci/install_apoc_docker ./ci || exit 1
-ch-convert -i ch-image -o tar apoc_neo4j $NEO4J_CONTAINER || exit 1
+# Build the Neo4j container
+ch-image build -t neo4j_image -f ./beeflow/data/dockerfiles/Dockerfile.neo4j ./ci || exit 1
+ch-convert -i ch-image -o tar neo4j_image $NEO4J_CONTAINER || exit 1
 # Pull the Redis container
 ch-image pull redis || exit 1
 ch-convert -i ch-image -o tar redis $REDIS_CONTAINER || exit 1
