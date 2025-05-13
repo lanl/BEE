d4b9e31ebd286780ab06c1d97c3448d545253664
Aaron R Hall <arhall@lanl.gov>
2025-05-13 10:53:08 -0600

    More tests on `bee_client`

--- beeflow/tests/test_bee_client.py
+++ beeflow/tests/test_bee_client.py
@@ -1,8 +1,17 @@
+import os
 import pytest
 import jsonpickle
 import logging
 from beeflow.client import bee_client
 from beeflow.common.db import client_db
+from requests.exceptions import ConnectionError
+
+
+@pytest.fixture(autouse=True)
+def reset_short_id_len():
+    original = bee_client.short_id_len
+    yield
+    bee_client.short_id_len = original
 
 
 def test_clienterror():
@@ -233,6 +242,82 @@ def test_check_hostname(
 
 
 @pytest.mark.parametrize(
+    "msg, include_caller, exp_err",
+    [
+        ("test msg", True, "Test_error_exit: test msg"),
+        ("test msg", False, "test msg"),
+    ],
+)
+def test_error_exit(mocker, msg, include_caller, exp_err):
+    """Regression test error_exit."""
+    mocker.patch("beeflow.client.bee_client._INTERACTIVE", False)
+    with pytest.raises(bee_client.ClientError, match=exp_err):
+        bee_client.error_exit(msg, include_caller)
+
+
+@pytest.mark.parametrize(
+    "msg, include_caller, exp_err",
+    [
+        ("test msg", True, "Test_error_exit_int: test msg\n"),
+        ("test msg", False, "test msg\n"),
+    ],
+)
+def test_error_exit_int(capsys, mocker, msg, include_caller, exp_err):
+    """Regression test error_exit for interactive."""
+    mocker.patch("beeflow.client.bee_client._INTERACTIVE", True)
+    sys_exit = mocker.patch("sys.exit")
+    bee_client.error_exit(msg, include_caller)
+    cap = capsys.readouterr()
+    sys_exit.assert_called_once_with(1)
+    assert cap.err == exp_err
+
+
+@pytest.mark.parametrize(
+    "status_code, key, exp_ret, exp_file",
+    [
+        (200, "", True, ""),
+        (500, "some key", True, ""),
+        (500, "error", False, "args: -f my_file.p\n"),
+    ],
+)
+def test_error_handler(tmpdir, mocker, status_code, key, exp_ret, exp_file):
+    error_exit = mocker.patch("beeflow.client.bee_client.error_exit")
+    mocker.patch("time.time", return_value=1)
+    mocker.patch("sys.argv", ["-f", "my_file.p"])
+    fake_resp = mocker.Mock()
+    fake_resp.status_code = status_code
+    fake_resp.json.return_value = {key: ""}
+    with tmpdir.as_cwd():
+        ret = bee_client.error_handler(fake_resp)
+        if exp_ret:
+            # it just returns the response we gave
+            assert ret == fake_resp
+        else:
+            assert ret == None
+        if exp_file:
+            assert os.path.isfile("bee-error-1.log")
+            with open("bee-error-1.log", "r", encoding="utf-8") as f:
+                file_cont = f.read()
+            assert file_cont == exp_file
+
+
+def test_wfm_conn(mocker):
+    """Regression test _wfm_conn."""
+    Connection = mocker.patch("beeflow.client.bee_client.Connection")
+    mocker.patch("beeflow.common.paths.wfm_socket", return_value="/fake/path/")
+    bee_client._wfm_conn()
+    Connection.assert_called_once_with(
+        "/fake/path/", error_handler=bee_client.error_handler
+    )
+
+
+def test_resource():
+    """Regression test _resource."""
+    ret = bee_client._resource(tag="tag")
+    assert ret == "bee_wfm/v1/jobs/tag"
+
+
+@pytest.mark.parametrize(
     "input_data",
     [([["checkpoint_test", "8df4418a6d1b42f2bda87c7a07f2c00d", "Archived"]])],
 )
@@ -248,6 +333,94 @@ def test_get_wf_list(mocker, input_data):
     result = bee_client.get_wf_list()
     # this should just decode back to the data we gave it
     assert result == input_data
+
+
+def test_get_wf_list_except(mocker):
+    """Regression test get_wf_list when ConnectionError"""
+    mock_conn = mocker.Mock()
+    mock_conn.get.side_effect = ConnectionError()
+    mocker.patch("beeflow.client.bee_client.get_hostname", return_value="test_hostname")
+    mocker.patch("beeflow.client.bee_client._wfm_conn", return_value=mock_conn)
+    with pytest.raises(
+        bee_client.ClientError, match="Get_wf_list: Could not reach WF Manager."
+    ):
+        result = bee_client.get_wf_list()
+
+
+def test_get_wf_list_except_exit(mocker):
+    """Regression test get_wf_list when ConnectionError and no hostname."""
+    warn = mocker.patch("beeflow.client.bee_client.warn")
+    mock_conn = mocker.Mock()
+    mock_conn.get.side_effect = ConnectionError()
+    mocker.patch("beeflow.client.bee_client.get_hostname", return_value="")
+    mocker.patch("beeflow.client.bee_client._wfm_conn", return_value=mock_conn)
+    with pytest.raises(SystemExit):
+        bee_client.get_wf_list()
+    warn.assert_called_once_with("beeflow has not been started!")
+
+
+def test_get_wf_list_500(mocker):
+    """Regression test get_wf_list with error code."""
+    fake_resp = mocker.Mock()
+    fake_resp.status_code = 500
+    mock_conn = mocker.Mock()
+    mock_conn.get.return_value = fake_resp
+    mocker.patch("beeflow.client.bee_client._wfm_conn", return_value=mock_conn)
+    with pytest.raises(
+        bee_client.ClientError,
+        match="Get_wf_list: WF Manager did not return workflow list",
+    ):
+        result = bee_client.get_wf_list()
+
+
+@pytest.mark.parametrize(
+    "id_list, exp_id_len",
+    [
+        (
+            [
+                "acd96a4ece434649abee1622c80e39e1",
+                "c34e865453cf43b1b3759c60c78c43c1",
+                "8df4418a6d1b42f2bda87c7a07f2c00d",
+                "a5554c4138294427a9eb0a9f89215cee",
+                "e50d8669595a4b05b841bc7b5336c721",
+            ],
+            6,
+        ),
+        (
+            [
+                "123456" + "ece434649abee1622c80e39e1",
+                "123456" + "5453cf43b1b3759c60c78c43c1",
+                "123456" + "8a6d1b42f2bda87c7a07f2c00d",
+                "123456" + "4138294427a9eb0a9f89215cee",
+                "123456" + "69595a4b05b841bc7b5336c721",
+            ],
+            7,
+        ),
+    ],
+)
+def test_check_short_id_collision(mocker, id_list, exp_id_len):
+    """Regression test check_short_id_collision."""
+    workflow_list = [["", this_id] for this_id in id_list]
+    mocker.patch("beeflow.client.bee_client.get_wf_list", return_value=workflow_list)
+    bee_client.check_short_id_collision()
+    assert bee_client.short_id_len == exp_id_len
+
+
+def test_check_short_id_collision_runtime(mocker):
+    workflow_list = [["", "acd96a4ece434649abee1622c80e39e1"] for i in range(3)]
+    mocker.patch("beeflow.client.bee_client.get_wf_list", return_value=workflow_list)
+    with pytest.raises(
+        RuntimeError, match="collision detected between two full workflow IDs"
+    ):
+        bee_client.check_short_id_collision()
+
+
+def test_check_short_id_collision_no_jobs(mocker, capsys):
+    """Regression test check_short_id_collision when no jobs."""
+    mocker.patch("beeflow.client.bee_client.get_wf_list", return_value=[])
+    bee_client.check_short_id_collision()
+    cap = capsys.readouterr()
+    assert cap.out == "There are currently no jobs.\n"
 
 
 @pytest.mark.parametrize(
