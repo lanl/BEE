ede638569dc3c0a0cd4ccc077a7348df26e1367b
Rusty Davis <rustyd@lanl.gov>
2025-10-22 16:34:59 -0600

    Fix networkx issue when numpy is also installed (#1142)
    * Fix networkx issue when numpy is also installed in the python environment
    
    * Pin pylint version
    
    * Pin github actions CI pylint version
    
    * Make sure integration tests and unit-tests install the correct version of python 3.11 to 3.14
    
    * Add cffi for flux.
    
    * Add python3 to pip call so we don't accidentally call the system python.
    
    * Further use the correct pip version
    
    * Add verbose debugging
    
    * Fix python version in documentation

---
+++ ci/bee_config_scaling.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+# BEE Configuration
+
+case $BEE_WORKER in
+Slurm*)
+    export WORKLOAD_SCHEDULER=Slurm
+    ;;
+Flux)
+    export WORKLOAD_SCHEDULER=Flux
+    ;;
+esac
+
+mkdir -p $(dirname $BEE_CONFIG)
+cat >> $BEE_CONFIG <<EOF
+# BEE CONFIGURATION FILE #
+[DEFAULT]
+bee_workdir = $BEE_WORKDIR
+bee_archive_dir = $BEE_WORKDIR/archives
+bee_droppoint = $BEE_WORKDIR/droppoint
+workload_scheduler = $WORKLOAD_SCHEDULER
+neo4j_image = $NEO4J_CONTAINER
+redis_image = $REDIS_CONTAINER
+max_restarts = 2
+delete_completed_workflow_dirs = True
+
+[task_manager]
+jobs_limit =
+container_runtime = Charliecloud
+runner_opts =
+background_interval = 2
+
+[charliecloud]
+image_mntdir = /tmp
+chrun_opts = --home
+setup =
+
+[job]
+default_account =
+default_time_limit =
+default_partition = scaling
+default_qos=
+default_reservation=
+
+[graphdb]
+hostname = localhost
+dbpass = password
+gdb_image_mntdir = /tmp
+sleep_time = 10
+
+[scheduler]
+algorithm = fcfs
+default_algorithm = fcfs
+
+[workflow_manager]
+
+[builder]
+deployed_image_root = /tmp
+container_output_path = /tmp
+container_type = charliecloud
+container_archive = $BEE_WORKDIR/container_archive
+EOF
+
+case $BEE_WORKER in
+Slurmrestd)
+    cat >> $BEE_CONFIG <<EOF
+[slurm]
+use_commands = False
+EOF
+    ;;
+SlurmCommands)
+    cat >> $BEE_CONFIG <<EOF
+[slurm]
+use_commands = True
+EOF
+    ;;
+esac
+
+case $BEE_WORKER in
+Slurmrestd)
+	cat >> $BEE_CONFIG << EOF
+[slurm attributes]
+attributes = $BEE_ATTR
+EOF
+	;;
+SlurmCommands)
+	cat >> $BEE_CONFIG << EOF
+[slurm command attributes]
+attributes = $BEE_ATTR
+EOF
+	;;
+Flux)
+	cat >> $BEE_CONFIG << EOF
+[flux attributes]
+attributes = $BEE_ATTR
+EOF
+	;;
+esac
+
+printf "\n\n"
+printf "#### %s ####\n" $BEE_CONFIG
+cat $BEE_CONFIG
+printf "#### %s ####\n" $BEE_CONFIG
+printf "\n\n"
