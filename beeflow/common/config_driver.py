c5ce1b7e9f2cfdb7ba4e80aee504c6da2020cc36
Rusty Davis <rustyd@lanl.gov>
2025-12-15 09:46:49 -0700

    Add full spack support.

--- beeflow/common/config_driver.py
+++ beeflow/common/config_driver.py
@@ -269,6 +269,10 @@ VALIDATOR.option('DEFAULT', 'workload_scheduler', choices=('Slurm', 'LSF', 'Flux
 VALIDATOR.option('DEFAULT', 'delete_completed_workflow_dirs', validator=validation.bool_,
                  default=True, info='delete workflow directory for completed jobs', prompt=False)
 
+VALIDATOR.option('DEFAULT', 'use_redis_container', validator=validation.bool_,
+                 default=True, info='Use the redis container image or spack',
+                 prompt=False)
+
 VALIDATOR.option('DEFAULT', 'neo4j_image', validator=validation.file_,
                  default=NEO4J_IMAGE, info='neo4j container image',
                  input_fn=filepath_completion_input, prompt=True)
@@ -276,6 +280,10 @@ VALIDATOR.option('DEFAULT', 'neo4j_image', validator=validation.file_,
 VALIDATOR.option('DEFAULT', 'redis_image', validator=validation.file_,
                  default=REDIS_IMAGE, info='redis container image',
                  input_fn=filepath_completion_input, prompt=True)
+
+VALIDATOR.option('DEFAULT', 'spack_path', validator=validation.dir_,
+                 info='Spack environment path',
+                 input_fn=filepath_completion_input, prompt=False)
 
 VALIDATOR.option('DEFAULT', 'max_restarts', validator=int, default=3, prompt=False,
                  info='max number of times beeflow will restart a component on failure')
--- beeflow/common/deps/redis_manager.py
+++ beeflow/common/deps/redis_manager.py
@@ -1,43 +1,71 @@
 """Module contains the code for launching redis subprocess."""
 import os
+import shutil
 import subprocess
 
 from beeflow.common import paths
+from beeflow.common.deps import container_manager
+from beeflow.common.config_driver import BeeConfig as bc
+
+def setup_config(container):
+    """Setup the redis config."""
+    data_dir = 'data'
+    os.makedirs(os.path.join(paths.redis_root(), data_dir), exist_ok=True)
+    conf_name = 'redis.conf'
+    conf_path = os.path.join(paths.redis_root(), conf_name)
+    if container:
+        mount_point = '/mnt'
+    else:
+        mount_point = paths.redis_root()
+    with open(conf_path, 'w', encoding='utf-8') as fp:
+        # Don't listen on TCP
+        print('port 0', file=fp)
+        print('dir', os.path.join(mount_point, data_dir), file=fp)
+        print('maxmemory 2mb', file=fp)
+        print('unixsocket', os.path.join(mount_point, paths.redis_sock_fname()), file=fp)
+        print('unixsocketperm 700', file=fp)
+    return conf_path
 
 
 def start(log):
     """Start redis."""
-    data_dir = 'data'
-    os.makedirs(os.path.join(paths.redis_root(), data_dir), exist_ok=True)
-    conf_name = 'redis.conf'
-    container_path = paths.redis_container()
-    # Dump the config
-    conf_path = os.path.join(paths.redis_root(), conf_name)
-    if not os.path.exists(conf_path):
-        with open(conf_path, 'w', encoding='utf-8') as fp:
-            # Don't listen on TCP
-            print('port 0', file=fp)
-            print('dir', os.path.join('/mnt', data_dir), file=fp)
-            print('maxmemory 2mb', file=fp)
-            print('unixsocket', os.path.join('/mnt', paths.redis_sock_fname()), file=fp)
-            print('unixsocketperm 700', file=fp)
-    # cmd = [
-    #     'ch-run',
-    #     f'--bind={paths.redis_root()}:/mnt',
-    #     container_path,
-    #     'redis-server',
-    #     '/mnt/redis.conf',
-    # ]
+    use_containers = bc.get("DEFAULT", "use_redis_container")
+    if use_containers:
+        container_path = paths.redis_container()
+        if not container_manager.check_container_dir('redis'):
+            print('Unpacking Redis image...')
+            container_manager.create_image('redis')
+        setup_config(container=True)
+        cmd = [
+            'ch-run',
+            f'--bind={paths.redis_root()}:/mnt',
+            container_path,
+            'redis-server',
+            '/mnt/redis.conf',
+        ]
 
-    cmd = [
-        'redis-server',
-        conf_path
-    ]
+        # Ran into a strange "Failed to configure LOCALE for invalid locale name."
+        # from Redis, so setting LANG=C. This could have consequences for UTF-8
+        # strings.
+        env = dict(os.environ)
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        proc = subprocess.Popen(cmd, env=env, stdout=log, stderr=log)
+    else:
+        conf_path = setup_config(container=False) 
+        redis_exists = shutil.which('redis-server')
+        cmd = ['redis-server', conf_path]
 
-    # Ran into a strange "Failed to configure LOCALE for invalid locale name."
-    # from Redis, so setting LANG=C. This could have consequences for UTF-8
-    # strings.
-    env = dict(os.environ)
-    env['LANG'] = 'C'
-    env['LC_ALL'] = 'C'
-    return subprocess.Popen(cmd, env=env, stdout=log, stderr=log)
+        if not redis_exists:
+            spack_path = bc.get("DEFAULT", "spack_path")
+            spec = "redis"
+            # Need to add redis version checking
+            bash_cmd = f'''
+                source "{spack_path}/share/spack/setup-env.sh"
+                spack load {spec}
+                exec {" ".join(cmd)} 
+            '''
+            proc = subprocess.Popen(["bash", "-lc", bash_cmd], stdout=log, stderr=log)
+        else:
+            proc = subprocess.Popen(cmd, stdout=log, stderr=log)
+    return proc
