779ea13d67aec3bf1ea808a8891b354fd97f5902
Wesley Mason <wmason@lanl.gov>
2026-03-03 08:04:54 -0700

    * Create cgroup.conf to disable cgroup plugin.  Try installing necessary dependencies for building cgroup/v2 plugin.

--- ci/deps_install.sh
+++ ci/deps_install.sh
@@ -5,10 +5,11 @@ set -e
 sudo apt-get update
 sudo apt-get install -y libhttp-parser-dev libjson-c-dev libjwt-dev munge python3 python3-venv \
     curl build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev sqlite3 \
-    libssl-dev libsqlite3-dev libreadline-dev libffi-dev libbz2-dev libmunge-dev \
-    libyaml-dev # needed for PyYAML 
+    libssl-dev libsqlite3-dev libreadline-dev libffi-dev libbz2-dev libmunge-dev libdbus-1-dev \
+    graphviz libgraphviz-dev libyaml-dev # needed for PyYAML 
 
-sudo apt-get install -y graphviz libgraphviz-dev
+# Install linux headers
+sudo apt-get install linux-headers-$(uname -r)
 
 # Install most recent Charliecloud
 curl -O -L https://github.com/hpc/charliecloud/releases/download/v${CHARLIECLOUD_VERSION}/charliecloud-${CHARLIECLOUD_VERSION}.tar.gz
