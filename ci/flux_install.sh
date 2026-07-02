19c5b11d9d601fb2473dbec2f58f1e80eed7b817
Sare Beste Zeytun - 407210 <szeytun@darwin-fe2.lanl.gov>
2026-07-02 15:38:24 -0600

    Attempt #2 to speed up workflow more

--- ci/flux_install.sh
+++ ci/flux_install.sh
@@ -44,7 +44,7 @@ python3 -m pip install "cffi>=1.15" pycparser
 git clone --depth 1 -b v${FLUX_SECURITY_VERSION} https://github.com/flux-framework/flux-security.git
 (cd flux-security
  ./autogen.sh
- PYTHON=/usr/bin/python3.11 ./configure --prefix=/usr
+ PYTHON=/usr/bin/python3.11 ./configure --prefix=$GITHUB_WORKSPACE/deps
  make
  sudo make install
  sudo ldconfig)
@@ -53,7 +53,7 @@ git clone --depth 1 -b v${FLUX_SECURITY_VERSION} https://github.com/flux-framewo
 git clone --depth 1 -b v${FLUX_CORE_VERSION} https://github.com/flux-framework/flux-core.git
 (cd flux-core
  ./autogen.sh
- PYTHON=/usr/bin/python3.11 ./configure --prefix=/usr
+ PYTHON=/usr/bin/python3.11 ./configure --prefix=$GITHUB_WORKSPACE/deps
  make
  sudo make install
  sudo ldconfig)
