991317e597b31e0024bf89f9f053f1a00378b5fb
Sare Beste Zeytun - 407210 <szeytun@darwin-fe2.lanl.gov>
2026-07-27 16:46:25 -0600

    Make one of the files executable

--- ci/slurm_install.sh
+++ ci/slurm_install.sh
@@ -2,10 +2,27 @@
 # Install Slurm for BEE
 set -e
 
-curl -O -L https://download.schedmd.com/slurm/slurm-${SLURM_VERSION}.tar.bz2
-tar -xvf slurm-${SLURM_VERSION}.tar.bz2
-(cd slurm-${SLURM_VERSION}
+SLURM_ARCHIVE="slurm-${SLURM_VERSION}.tar.gz"
+SLURM_DIR="slurm-${SLURM_VERSION}"
+
+curl -fL \
+  --retry 5 \
+  --retry-delay 15 \
+  --retry-all-errors \
+  --connect-timeout 30 \
+  --max-time 300 \
+  -o "$SLURM_ARCHIVE" \
+  "https://github.com/SchedMD/slurm/archive/refs/tags/${SLURM_TAG}.tar.gz"
+
+mkdir "$SLURM_DIR"
+
+tar -xzvf "$SLURM_ARCHIVE" \
+    --strip-components=1 \
+    -C "$SLURM_DIR"
+(
+ cd "$SLURM_DIR"
  ./configure --prefix=$GITHUB_WORKSPACE/slurm --enable-cgroupv2
  grep -i cgroup config.log
  make -j4
- sudo make install)
+ sudo make install
+)
