eb2900b2bdd414899d67b21b4639ab578efbeaf7
Santiago Lopez <santiagolopez@lanl.gov>
2026-08-19 15:46:46 -0600

    This commit adds a bool val for use redis container for re-git

--- ci/bee_config_scaling.sh
+++ ci/bee_config_scaling.sh
@@ -20,6 +20,7 @@ bee_droppoint = $BEE_WORKDIR/droppoint
 workload_scheduler = $WORKLOAD_SCHEDULER
 neo4j_image = $NEO4J_CONTAINER
 redis_image = $REDIS_CONTAINER
+use_redis_container = False
 max_restarts = 2
 delete_completed_workflow_dirs = True
 
