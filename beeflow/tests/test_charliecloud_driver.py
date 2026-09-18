245b519064caa19598cd2bd480fee42cb2b4e60f
Patrica Grubel <pagrubel@lanl.gov>
2026-09-17 19:28:00 -0600

    Remove skip test - should improve coverage.

--- beeflow/tests/test_charliecloud_driver.py
+++ beeflow/tests/test_charliecloud_driver.py
@@ -4,9 +4,6 @@ import pytest
 from beeflow.common.crt.charliecloud_driver import CharliecloudDriver as crt_driver
 from beeflow.common.object_models import Task, Requirement
 
-pytestmark = pytest.mark.skip(
-    reason="Temporary coverage comment verification"
-)
 @pytest.mark.parametrize(
     "use_container, pre_commands_exp, main_command_exp, post_commands_exp",
     [
